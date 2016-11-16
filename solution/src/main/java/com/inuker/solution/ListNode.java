93b3b6395bf757e197622a10f5162a4bb33aef8e
dingjikerbo <dingjikerbo@gmail.com>
2016-11-16 20:09:43 +0800

    reverse linked list

---
+++ solution/src/main/java/com/inuker/solution/ListNode.java
@@ -0,0 +1,15 @@
+package com.inuker.solution;
+
+/**
+ * Created by liwentian on 2016/11/16.
+ */
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
