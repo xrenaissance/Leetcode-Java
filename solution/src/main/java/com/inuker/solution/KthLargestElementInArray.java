0341ef182299f45477b0f7ee4d494195ba4cb07c
dingjikerbo <dingjikerbo@gmail.com>
2016-11-17 10:45:31 +0800

    kth largest element in array

---
+++ solution/src/main/java/com/inuker/solution/KthLargestElementInArray.java
@@ -0,0 +1,107 @@
+package com.inuker.solution;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+
+/**
+ * Created by dingjikerbo on 2016/11/17.
+ */
+
+public class KthLargestElementInArray {
+
+    // 耗时16ms，时间复杂度O(nlgn)，空间复杂度O(n)
+    // 按降序排的，出队列k次获取第k大的数
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 > o2 ? -1 : 1;
+            }
+        });
+
+        for (int n : nums) {
+            queue.add(n);
+        }
+
+        int num = 0;
+
+        for (int i = 0; i < k; i++) {
+            num = queue.poll();
+        }
+
+        return num;
+    }
+
+    // 耗时17ms，时间复杂度O(nlgk)，空间复杂度O(k)
+    // 按升序排的，最后队列头是最后的k个数中最小的，也就是全局第k大的
+    public int findKthLargest2(int[] nums, int k) {
+        Queue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 > o2 ? 1 : -1;
+            }
+        });
+
+        for (int n : nums) {
+            queue.add(n);
+
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+
+        return queue.peek();
+    }
+
+    // 耗时3ms，时间复杂度O(nlgn)，空间复杂度O(l)
+    public int findKthLargest3(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    // 快速选择，平均时间复杂度为O(n)
+    // T(n) = T(n / 2) + n = O(n)
+    // 对比快速排序T(n) = 2T(n / 2) + n = O(nlgn)
+    // 区别在于这个被pivot分隔后，只用处理其中的一半，而快排两边都要处理
+    // 耗时18ms
+    public int findKthLargest4(int[] nums, int k) {
+        return nums[findKthLargest4(nums, 0, nums.length - 1, k)];
+    }
+
+    public int findKthLargest4(int[] nums, int start, int end, int k) {
+        int pivot = partition(nums, start, end);
+
+        int rank = end - pivot + 1;
+
+        if (rank == k) {
+            return pivot;
+        } else if (rank > k) {
+            return findKthLargest4(nums, pivot + 1, end, k);
+        } else {
+            return findKthLargest4(nums, start, pivot - 1, k - rank);
+        }
+    }
+
+    public int partition(int[] nums, int start, int end) {
+        int pivot = nums[end], left = start;
+
+        for (int i = start; i < end; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, left++, i);
+            }
+        }
+
+        swap(nums, left, end);
+
+        return left;
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int t = nums[left];
+        nums[left] = nums[right];
+        nums[right] = t;
+    }
+}
--- solution/src/main/java/com/inuker/solution/Main.java
+++ solution/src/main/java/com/inuker/solution/Main.java
@@ -7,13 +7,18 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        Set<String> set = new HashSet<String>();
-        set.add("hot");
-        set.add("dot");
-        set.add("dog");
-        set.add("lot");
-        set.add("log");
-        System.out.println(new WordLadder().ladderLength2("hit", "cog", set));
+        int[] arr = {
+                6, 3, 8, 2, 5, 7, 9
+        };
+
+        int k = new KthLargestElementInArray().findKthLargest4(arr, 2);
+
+        System.out.println(k);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
 //        StringBuilder sb = new StringBuilder();
 //        for (int i = 0; i < 29999; i++) {
 //            sb.append("a");
