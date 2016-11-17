d57ec8e6395207bc802dea4d1a0b1dd519095958
dingjikerbo <dingjikerbo@gmail.com>
2016-11-17 16:59:27 +0800

    validate binary search tree

---
+++ solution/src/main/java/com/inuker/solution/ValidateBinarySearchTree.java
@@ -0,0 +1,22 @@
+package com.inuker.solution;
+
+/**
+ * Created by dingjikerbo on 2016/11/17.
+ */
+
+public class ValidateBinarySearchTree {
+
+    // 耗时1ms
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, (long) Integer.MIN_VALUE - 1, (long) Integer.MAX_VALUE + 1);
+    }
+
+    private boolean isValidBST(TreeNode root, long lower, long upper) {
+        if (root == null) {
+            return true;
+        }
+        return root.val > lower && root.val < upper
+                && isValidBST(root.left, lower, root.val)
+                && isValidBST(root.right, root.val, upper);
+    }
+}
