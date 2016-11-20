76f99a53297ef0d0bcb2f4566db95c084f22807e
liwentian <liwentian@xiaomi.com>
2016-11-20 16:04:15 +0800

    regular expression matching

--- solution/src/main/java/com/inuker/solution/Main.java
+++ solution/src/main/java/com/inuker/solution/Main.java
@@ -8,19 +8,7 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        char[] buf = new char[128];
-        int size = new ReadNCharactersGivenRead4II().read(buf, 1);
-        System.out.println(size);
-        for (int i = 0; i < size; i++) {
-            System.out.print(buf[i] + " ");
-        }
-        System.out.println();
-        size = new ReadNCharactersGivenRead4II().read(buf, 2);
-        System.out.println(size);
-        for (int i = 0; i < size; i++) {
-            System.out.print(buf[i] + " ");
-        }
-
+        System.out.print(new RegularExpressionMatching().isMatch("aa", "a"));
     }
 
 
---
+++ solution/src/main/java/com/inuker/solution/RegularExpressionMatching.java
@@ -0,0 +1,45 @@
+package com.inuker.solution;
+
+/**
+ * Created by dingjikerbo on 16/11/20.
+ */
+/**
+ * 这道题最重要的是no bug，而不是性能
+ * 面试中要给所有cases cover到
+ * 下面的写法足够简单和直观了
+ * DP也可，但是不推荐
+ */
+public class RegularExpressionMatching {
+
+    // 耗时153ms
+    public boolean isMatch(String s, String p) {
+        // 如果p是空串，那么s必须也是空串
+        if (p.length() == 0) {
+            return s.length() == 0;
+        }
+
+        // 如果p只有一个字符，那么s必须也是1个字符，而且两者要匹配，注意p不会是单独的*，因为*要跟在某个字符后面
+        if (p.length() == 1) {
+            return s.length() == 1 && isMatch(s, 0, p);
+        }
+
+        if (p.charAt(1) != '*') {
+            if (s.length() < 1) {
+                return false;
+            }
+
+            return isMatch(s, 0, p) && isMatch(s.substring(1), p.substring(1));
+        } else {
+            // 这里要注意判断s是否为空
+            if (s.length() > 0 && isMatch(s, 0, p)) {
+                return isMatch(s, p.substring(2)) || isMatch(s.substring(1), p);
+            } else {
+                return isMatch(s, p.substring(2));
+            }
+        }
+    }
+
+    private boolean isMatch(String s, int i, String p) {
+        return s.charAt(i) == p.charAt(0) || p.charAt(0) == '.';
+    }
+}
