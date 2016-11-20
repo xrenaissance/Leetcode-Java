82d45e57f9163b6e943101d4fb836486a38b2be6
liwentian <liwentian@xiaomi.com>
2016-11-20 17:49:01 +0800

    meeting rooms

---
+++ solution/src/main/java/com/inuker/solution/Interval.java
@@ -0,0 +1,24 @@
+package com.inuker.solution;
+
+/**
+ * Created by dingjikerbo on 16/11/20.
+ */
+
+/**
+ * Definition for an interval.
+ */
+public class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
+
