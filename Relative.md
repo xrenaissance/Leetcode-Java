d32dabae41710068ff67b6eadb4c622801873713
liwentian <liwentian@xiaomi.com>
2016-11-26 15:42:19 +0800

    range sum query inmutable

---
+++ Relative.md
@@ -0,0 +1,7 @@
+相似题目列表
+------
+
+Range Sum Query
+|#|Title|Solution|
+|---|----| ----- |
+|303|[Range Sum Query - Immutable](https://leetcode.com/problems/range-sum-query-immutable/)| [Java](https://github.com/dingjikerbo/leetcode/blob/master/solution/src/main/java/com/inuker/solution/NumArray.java)|
---
+++ solution/src/main/java/com/inuker/solution/NumMatrix.java
@@ -0,0 +1,26 @@
+package com.inuker.solution;
+
+/**
+ * Created by dingjikerbo on 16/11/26.
+ */
+
+public class NumMatrix {
+
+    public NumMatrix(int[][] matrix) {
+
+    }
+
+    public void update(int row, int col, int val) {
+
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+    }
+}
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix = new NumMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.update(1, 1, 10);
+// numMatrix.sumRegion(1, 2, 3, 4);
