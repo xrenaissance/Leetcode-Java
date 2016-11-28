1818392d48a6039ae28a19d9e5ff2ddf7fd37fe8
liwentian <liwentian@xiaomi.com>
2016-11-28 21:04:24 +0800

    minimum depth of binary tree

---
+++ solution/src/main/java/com/inuker/solution/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,38 @@
+package com.inuker.solution;
+
+/**
+ * Created by dingjikerbo on 16/11/28.
+ */
+
+public class MinimumDepthOfBinaryTree {
+
+    /**
+     * 错误的写法，和第104题非常类似
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }*/
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        // 如果一边空了，那这边深度为0，但是由于不是叶子节点，所以不能算，得看另一边。
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        } else if (root.right == null) {
+            return minDepth(root.left) + 1;
+        } else {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+}
