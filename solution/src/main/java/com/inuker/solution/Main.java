139908d1bb8857695a1b3e692e19747c636288f8
liwentian <liwentian@xiaomi.com>
2016-12-01 20:52:55 +0800

    count univalue subtrees

--- solution/src/main/java/com/inuker/solution/Main.java
+++ solution/src/main/java/com/inuker/solution/Main.java
@@ -8,22 +8,8 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode node2 = new TreeNode(2, null, null);
-        TreeNode node1 = new TreeNode(1, node2, null);
-
-        new FlattenBinaryTreeToLinkedList().flatten(node1);
-        while (node1 != null) {
-            System.out.print(node1.val + " ");
-            node1 = node1.right;
-        }
+        boolean flag = true | true;
+        System.out.println(flag);
     }
 
-    private static void helper(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        helper(root.left);
-        System.out.println(root.val);
-        helper(root.right);
-    }
 }
