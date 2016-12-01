b1fdadcc381807167b624d48113de60c4108d49e
liwentian <liwentian@xiaomi.com>
2016-12-02 07:22:54 +0800

    binary tree upside down

---
+++ solution/src/main/java/com/inuker/solution/BinaryTreeUpsideDown.java
@@ -0,0 +1,20 @@
+package com.inuker.solution;
+
+/**
+ * Created by dingjikerbo on 16/12/2.
+ */
+
+public class BinaryTreeUpsideDown {
+
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;   // node 2 left children
+        root.left.right = root;         // node 2 right children
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
