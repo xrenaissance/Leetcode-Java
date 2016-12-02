a080ac28282cc7347d7fe1216baa2762a8822245
dingjikerbo <dingjikerbo@gmail.com>
2016-12-02 09:42:09 +0800

    sum of left leaves

---
+++ solution/src/main/java/com/inuker/solution/SumOfLeftLeaves.java
@@ -0,0 +1,24 @@
+package com.inuker.solution;
+
+/**
+ * Created by liwentian on 2016/12/2.
+ */
+
+public class SumOfLeftLeaves {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = 0;
+
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            left = root.left.val;
+        } else {
+            left = sumOfLeftLeaves(root.left);
+        }
+
+        return left + sumOfLeftLeaves(root.right);
+    }
+}
