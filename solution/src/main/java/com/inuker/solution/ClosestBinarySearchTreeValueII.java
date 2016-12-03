73a0e7d7781e591773824ca18d6cdebb09726752
liwentian <liwentian@xiaomi.com>
2016-12-03 16:22:49 +0800

    find leaves of binary tree

--- solution/src/main/java/com/inuker/solution/ClosestBinarySearchTreeValueII.java
+++ solution/src/main/java/com/inuker/solution/ClosestBinarySearchTreeValueII.java
@@ -11,6 +11,6 @@ public class ClosestBinarySearchTreeValueII {
     // https://discuss.leetcode.com/topic/22940/ac-clean-java-solution-using-two-stacks/2
     // https://discuss.leetcode.com/topic/23151/o-logn-java-solution-with-two-stacks-following-hint
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
-
+        return null;
     }
 }
