4d2ae6f60b620965d821059b3956e32f57adef6e
dingjikerbo <dingjikerbo@gmail.com>
2016-12-05 11:47:10 +0800

    closest binary search tree value ii

--- solution/src/main/java/com/inuker/solution/ClosestBinarySearchTreeValueII.java
+++ solution/src/main/java/com/inuker/solution/ClosestBinarySearchTreeValueII.java
@@ -1,5 +1,9 @@
 package com.inuker.solution;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,7 +14,46 @@ public class ClosestBinarySearchTreeValueII {
 
     // https://discuss.leetcode.com/topic/22940/ac-clean-java-solution-using-two-stacks/2
     // https://discuss.leetcode.com/topic/23151/o-logn-java-solution-with-two-stacks-following-hint
+
+    // 耗时5ms，时间复杂度O(n)
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
-        return null;
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        inorderTraversal(root, list);
+
+        int index = -1;
+        double min = Double.MAX_VALUE;
+        for (int i = 0; i < list.size(); i++) {
+            double delta = Math.abs(list.get(i).val - target);
+            if (delta < min) {
+                min = delta;
+                index = i;
+            } else {
+                break;
+            }
+        }
+
+        List<Integer> result = new LinkedList<Integer>();
+        result.add(list.get(index).val);
+
+        for (int i = index - 1, j = index + 1; result.size() < k; ) {
+            double delta1 = i >= 0 ? Math.abs(list.get(i).val - target) : Double.MAX_VALUE;
+            double delta2 = j < list.size() ? Math.abs(list.get(j).val - target) : Double.MAX_VALUE;
+            if (delta1 > delta2) {
+                result.add(list.get(j++).val);
+            } else {
+                result.add(list.get(i--).val);
+            }
+        }
+
+        return result;
+    }
+
+    private void inorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, list);
+        list.add(root);
+        inorderTraversal(root.right, list);
     }
 }
--- solution/src/main/java/com/inuker/solution/Main.java
+++ solution/src/main/java/com/inuker/solution/Main.java
@@ -1,17 +1,46 @@
 package com.inuker.solution;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         TreeNode root = TreeNode.buildTree(new Integer[] {
-                1, -2, -3, 1, 3, -2, null, -1
+               7,6,8,4,null,null,12,3,5
         });
-        int count = new PathSumIII().pathSum(root, -1);
-        System.out.println(String.format("count is %d", count));
+//        int count = new PathSumIII().pathSum(root, -1);
+//        System.out.println(String.format("count is %d", count));
+
+        ArrayList<Integer> s1 = new ArrayList<>(); // predecessors
+        ArrayList<Integer> s2 = new ArrayList<>(); // successors
+
+        inorder(root, 4, false, s1);
+
+        for (Integer n : s1) {
+            System.out.print(n + " ");
+        }
+
+        inorder(root, 4, true, s2);
+        System.out.println();
+
+        for (Integer n : s2) {
+            System.out.print(n + " ");
+        }
+    }
+
+    static void inorder(TreeNode root, double target, boolean reverse, ArrayList<Integer> list) {
+        if (root == null) return;
+
+        inorder(reverse ? root.right : root.left, target, reverse, list);
+        // early terminate, no need to traverse the whole tree
+        if ((reverse && root.val <= target) || (!reverse && root.val > target)) return;
+        // track the value of current node
+        list.add(root.val);
+        inorder(reverse ? root.left : root.right, target, reverse, list);
     }
 }
