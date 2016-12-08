bc60fbe03cea261be779185e7ee9c9376fc68bb5
liwentian <liwentian@xiaomi.com>
2016-12-09 07:29:19 +0800

    contains duplicate

---
+++ solution/src/main/java/com/inuker/solution/ContainsDuplicateIII.java
@@ -0,0 +1,15 @@
+package com.inuker.solution;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by dingjikerbo on 16/12/8.
+ */
+
+public class ContainsDuplicateIII {
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+    }
+}
