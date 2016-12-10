456f1b56891d05f9f51fdd13b2ddfc2bb0f912c5
liwentian <liwentian@xiaomi.com>
2016-12-10 15:56:50 +0800

    wiggle sort ii

---
+++ solution/src/main/java/com/inuker/solution/WiggleSortII.java
@@ -0,0 +1,42 @@
+package com.inuker.solution;
+
+import java.util.Arrays;
+
+/**
+ * Created by dingjikerbo on 16/12/10.
+ */
+
+/**
+ * https://leetcode.com/articles/wiggle-sort/
+ */
+public class WiggleSortII {
+
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i + 1 < nums.length; i += 2) {
+            swap(nums, i, i + 1);
+        }
+    }
+
+    // 复杂度O(n)
+    public void wiggleSort2(int[] nums) {
+        boolean asc = true;
+        for (int i = 0; i + 1 < nums.length; i++, asc = !asc) {
+            if (asc) {
+                if (nums[i] > nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+            } else {
+                if (nums[i] < nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}
