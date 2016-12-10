87ddb177c65eb7aa8190db4a53152a1910c8d279
liwentian <liwentian@xiaomi.com>
2016-12-10 13:52:21 +0800

    fd

--- solution/src/main/java/com/inuker/solution/TopKFrequentElements.java
+++ solution/src/main/java/com/inuker/solution/TopKFrequentElements.java
@@ -16,14 +16,11 @@ import java.util.TreeMap;
 
 public class TopKFrequentElements {
 
+    // 耗时46ms
     public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
         for (int n : nums) {
-            if (map.containsKey(n)) {
-                map.put(n, map.get(n) + 1);
-            } else {
-                map.put(n, 1);
-            }
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
         Queue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
@@ -31,15 +28,36 @@ public class TopKFrequentElements {
                 return o2[1] - o1[1];
             }
         });
-        Iterator itor = map.keySet().iterator();
-        while (itor.hasNext()) {
-            int n = (int) itor.next();
-            queue.add(new int[] { n, map.get(n) });
+        for (int key : map.keySet()) {
+            queue.add(new int[] { key, map.get(key) });
         }
         List<Integer> list = new LinkedList<Integer>();
         for (int i = 1; i <= k && !queue.isEmpty(); i++) {
             list.add(queue.poll()[0]);
         }
         return list;
+    }
+
+    // 耗时23ms，复杂度O(n)
+    public List<Integer> topKFrequent2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        List<Integer>[] lists = new LinkedList[nums.length + 1];
+        for (int key : map.keySet()) {
+            int count = map.get(key);
+            if (lists[count] == null) {
+                lists[count] = new LinkedList<Integer>();
+            }
+            lists[count].add(key);
+        }
+        List<Integer> result = new LinkedList<Integer>();
+        for (int i = lists.length - 1; i >= 0 && result.size() < k; i--) {
+            if (lists[i] != null) {
+                result.addAll(lists[i]);
+            }
+        }
+        return result;
     }
 }
