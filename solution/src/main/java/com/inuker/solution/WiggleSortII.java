f1dd5019f41b5ffe113829855d74f2b45173b69f
liwentian <liwentian@xiaomi.com>
2016-12-10 15:59:55 +0800

    fd

--- solution/src/main/java/com/inuker/solution/WiggleSortII.java
+++ solution/src/main/java/com/inuker/solution/WiggleSortII.java
@@ -7,36 +7,64 @@ import java.util.Arrays;
  */
 
 /**
- * https://leetcode.com/articles/wiggle-sort/
+ * TestCase
+ * [1,2,2,1,2,1,1,1,1,2,2,2]
  */
 public class WiggleSortII {
 
     public void wiggleSort(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 1; i + 1 < nums.length; i += 2) {
-            swap(nums, i, i + 1);
+        int len = nums.length;
+        int median = findKthLargest(nums, (len + 1) / 2);
+        for (int left = 0, right = len - 1, i = 0; i <= right; ) {
+            if (nums[newIndex(i, len)] > median) {
+                swap(nums, newIndex(left++, len), newIndex(i++, len));
+            } else if (nums[newIndex(i, len)] < median) {
+                swap(nums, newIndex(right--, len), newIndex(i, len));
+            } else {
+                i++;
+            }
         }
     }
 
-    // 复杂度O(n)
-    public void wiggleSort2(int[] nums) {
-        boolean asc = true;
-        for (int i = 0; i + 1 < nums.length; i++, asc = !asc) {
-            if (asc) {
-                if (nums[i] > nums[i + 1]) {
-                    swap(nums, i, i + 1);
-                }
-            } else {
-                if (nums[i] < nums[i + 1]) {
-                    swap(nums, i, i + 1);
-                }
-            }
-        }
+    private int newIndex(int index, int n) {
+        return (1 + 2 * index) % (n | 1);
     }
 
     private void swap(int[] nums, int i, int j) {
         int t = nums[i];
         nums[i] = nums[j];
         nums[j] = t;
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        return nums[findKthLargest(nums, 0, nums.length - 1, k)];
+    }
+
+    public int findKthLargest(int[] nums, int start, int end, int k) {
+        int pivot = partition(nums, start, end);
+
+        int rank = end - pivot + 1;
+
+        if (rank == k) {
+            return pivot;
+        } else if (rank > k) {
+            return findKthLargest(nums, pivot + 1, end, k);
+        } else {
+            return findKthLargest(nums, start, pivot - 1, k - rank);
+        }
+    }
+
+    public int partition(int[] nums, int start, int end) {
+        int pivot = nums[end], left = start;
+
+        for (int i = start; i < end; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, left++, i);
+            }
+        }
+
+        swap(nums, left, end);
+
+        return left;
     }
 }
