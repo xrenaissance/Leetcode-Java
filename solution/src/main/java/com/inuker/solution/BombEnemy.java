a168d5439411c8fbdcdd94c6ad255cb482b31234
liwentian <liwentian@xiaomi.com>
2016-12-12 07:39:47 +0800

    fd

--- solution/src/main/java/com/inuker/solution/BombEnemy.java
+++ solution/src/main/java/com/inuker/solution/BombEnemy.java
@@ -19,7 +19,7 @@ public class BombEnemy {
         for (int i = 0; i < row; i++) {
             // 这个循环里每次都从墙或边界开始，是为了避免重复计算
             for (int j = 0; j < col; j++) {
-                // 如果当前是墙则直接跳过
+                /** 如果当前是墙则直接跳过 */
                 if (grid[i][j] == 'W') {
                     continue;
                 }
@@ -28,6 +28,7 @@ public class BombEnemy {
                 // 如果左边靠着墙或边界则统计往右延伸能杀多少敌人
                 // 如果不是靠着墙，则不用统计了，直接复用本行之前的结果
                 if (j == 0 || grid[i][j - 1] == 'W') {
+                    /** 注意这里rowhits要清零 */
                     rowhits = 0;
                     // 往右延伸一直到墙或边界
                     for (int k = j; k < col && grid[i][k] != 'W'; k++) {
@@ -39,6 +40,7 @@ public class BombEnemy {
                 // 如果上面靠着墙或边界则统计往下延伸能杀伤多少人
                 // 如果不是靠着墙则不用统计了，直接复用本列之前的结果
                 if (i == 0 || grid[i - 1][j] == 'W') {
+                    /** 注意这里colhits[j]要清零 */
                     colhits[j] = 0;
                     // 往下延伸到边界或者墙
                     for (int k = i; k < row && grid[k][j] != 'W'; k++) {
@@ -47,6 +49,7 @@ public class BombEnemy {
                 }
 
                 // 如果当前可以放炸弹，横向和纵向总杀伤人数和
+                /** 这里要判断是否可以放炸弹 */
                 if (grid[i][j] == '0') {
                     result = Math.max(result, rowhits + colhits[j]);
                 }
--- solution/src/main/java/com/inuker/solution/Solution.java
+++ solution/src/main/java/com/inuker/solution/Solution.java
@@ -6,6 +6,42 @@ package com.inuker.solution;
 
 public class Solution {
 
+    public int maxKilledEnemies(char[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length, col = grid[0].length;
+        int rowKill = 0, maxKill = 0;
+        int[] colKill = new int[col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 'W') {
+                    continue;
+                }
+                if (j == 0 || grid[i][j - 1] == 'W') {
+                    rowKill = 0;
+                    for (int k = j; k < col && grid[i][k] != 'W'; k++) {
+                        if (grid[i][k] == 'E') {
+                            rowKill++;
+                        }
+                    }
+                }
+                if (i == 0 || grid[i - 1][j] == 'W') {
+                    colKill[j] = 0;
+                    for (int k = i; k < row && grid[k][j] != 'W'; k++) {
+                        if (grid[k][j] == 'E') {
+                            colKill[j]++;
+                        }
+                    }
+                }
+                if (grid[i][j] == '0') {
+                    maxKill = Math.max(maxKill, rowKill + colKill[j]);
+                }
+            }
+        }
+        return maxKill;
+    }
+
     public int wordsTyping(String[] sentence, int rows, int cols) {
         String s = String.join(" ", sentence) + " ";
         int len = s.length(), start = 0;
