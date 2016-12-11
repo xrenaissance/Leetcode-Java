ea679bdd1a9f871615c29c7e6342c40effc02a5d
dingjikerbo <dingjikerbo@gmail.com>
2016-12-11 15:46:14 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/DecodeString.java
@@ -0,0 +1,12 @@
+package com.inuker.solution;
+
+/**
+ * Created by dingjikerbo on 2016/12/11.
+ */
+
+public class DecodeString {
+
+    public String decodeString(String s) {
+
+    }
+}
--- solution/src/main/java/com/inuker/solution/MissingRanges.java
+++ solution/src/main/java/com/inuker/solution/MissingRanges.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by liwentian on 2016/12/11.
+ * Created by dingjikerbo on 2016/12/11.
  */
 
 /**
