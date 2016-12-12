7b8e1dbebffaa30c81b11a1f037908593d3b5800
dingjikerbo <dingjikerbo@gmail.com>
2016-12-12 09:55:55 +0800

    fd

--- solution/src/main/java/com/inuker/solution/SurroundedRegions.java
+++ solution/src/main/java/com/inuker/solution/SurroundedRegions.java
@@ -69,7 +69,6 @@ public class SurroundedRegions {
      * BFS法
      * 给与边界的O相邻的所有O点都标为+，然后剩下的O肯定是不与边界O相邻的，则必然是被X包围的，
      * 将这些O标为X后，再给剩下的+都还原为O即可
-     * 代码不是太简洁，但是很容易懂，可以优化一下
      */
     public void solve2(char[][] board) {
         if (board.length == 0) {
@@ -78,49 +77,20 @@ public class SurroundedRegions {
         int row = board.length, col = board[0].length;
         Queue<int[]> queue = new LinkedList<int[]>();
         for (int i = 0; i < col; i++) {
-            if (board[0][i] == 'O') {
-                board[0][i] = '+';
-                queue.add(new int[]{0, i});
-            }
-            if (board[row - 1][i] == 'O') {
-                board[row - 1][i] = '+';
-                queue.add(new int[]{row - 1, i});
-            }
+            enqueue(board, 0, i, queue);
+            enqueue(board, row - 1, i, queue);
         }
         for (int i = 0; i < row; i++) {
-            if (board[i][0] == 'O') {
-                board[i][0] = '+';
-                queue.add(new int[] {i, 0});
-            }
-            if (board[i][col - 1] == 'O') {
-                board[i][col - 1] = '+';
-                queue.add(new int[] {i, col - 1});
-            }
+            enqueue(board, i, 0, queue);
+            enqueue(board, i, col - 1, queue);
         }
-
         while (!queue.isEmpty()) {
             int[] pos = queue.poll();
             int x = pos[0], y = pos[1];
-
-            if (x > 0 && board[x - 1][y] == 'O') {
-                board[x - 1][y] = '+';
-                queue.add(new int[] {x - 1, y});
-            }
-
-            if (x + 1 < row && board[x + 1][y] == 'O') {
-                board[x + 1][y] = '+';
-                queue.add(new int[] {x + 1, y});
-            }
-
-            if (y > 0 && board[x][y - 1] == 'O') {
-                board[x][y - 1] = '+';
-                queue.add(new int[] {x, y - 1});
-            }
-
-            if (y + 1 < col && board[x][y + 1] == 'O') {
-                board[x][y + 1] = '+';
-                queue.add(new int[] {x, y + 1});
-            }
+            enqueue(board, x - 1, y, queue);
+            enqueue(board, x + 1, y, queue);
+            enqueue(board, x, y + 1, queue);
+            enqueue(board, x, y - 1, queue);
         }
 
         for (int i = 0; i < row; i++) {
@@ -134,9 +104,16 @@ public class SurroundedRegions {
         }
     }
 
+    private void enqueue(char[][] board, int i, int j, Queue<int[]> queue) {
+        if (i >= 0 && i < board.length && j >= 0 && j < board[0].length && board[i][j] == 'O') {
+            board[i][j] = '+';
+            queue.add(new int[]{i, j});
+        }
+    }
+
     /**
      * DFS法
-     * 看起来代码简洁多了，不过当数据量较大时可能Stack Overflow
+     * 数据量大时可能Stack Overflow
      */
     public void solve3(char[][] board) {
         if (board.length == 0) {
