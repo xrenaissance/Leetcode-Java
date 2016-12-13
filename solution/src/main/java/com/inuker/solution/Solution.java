6ae9fb5d8cd4169f9946e869d31079d9253c42b9
dingjikerbo <dingjikerbo@gmail.com>
2016-12-13 19:58:03 +0800

    fd

--- solution/src/main/java/com/inuker/solution/Solution.java
+++ solution/src/main/java/com/inuker/solution/Solution.java
@@ -13,6 +13,30 @@ import java.util.Queue;
 
 public class Solution {
 
+    public int[][] multiply(int[][] A, int[][] B) {
+        int[][] result = new int[A.length][B[0].length];
+        for (int i = 0; i < A.length; i++) {
+            for (int k = 0; k < A[0].length; k++) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < B[0].length; j++) {
+                        result[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return result;
+
+//        int[][] result = new int[A.length][B[0].length];
+//        for (int i = 0; i < A.length; i++) {
+//            for (int j = 0; j < B[0].length; j++) {
+//                for (int k = 0; k < A[0].length; k++) {
+//                    result[i][j] += A[i][k] * B[k][j];
+//                }
+//            }
+//        }
+//        return result;
+    }
+
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> result = new LinkedList<List<Integer>>();
         if (root == null) {
--- solution/src/main/java/com/inuker/solution/SparseMatrixMultiplication.java
+++ solution/src/main/java/com/inuker/solution/SparseMatrixMultiplication.java
@@ -4,25 +4,34 @@ package com.inuker.solution;
  * Created by dingjikerbo on 2016/11/19.
  */
 
+/**
+ * 这题有点意思，方法一对稀疏矩阵会超时，只要给里层两个for循环交换一下顺序即可
+ */
 public class SparseMatrixMultiplication {
 
     public int[][] multiply(int[][] A, int[][] B) {
-        int M = A.length, T = B.length, N = B[0].length;
+        int[][] result = new int[A.length][B[0].length];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                for (int k = 0; k < A[0].length; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return result;
+    }
 
-        int[][] C = new int[M][N];
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < T; j++) {
-                if (A[i][j] != 0) {
-                    for (int k = 0; k < N; k++) {
-                        if (B[j][k] != 0) {
-                            C[i][k] += A[i][j] * B[j][k];
-                        }
+    public int[][] multiply2(int[][] A, int[][] B) {
+        int[][] result = new int[A.length][B[0].length];
+        for (int i = 0; i < A.length; i++) {
+            for (int k = 0; k < A[0].length; k++) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < B[0].length; j++) {
+                        result[i][j] += A[i][k] * B[k][j];
                     }
                 }
             }
         }
-
-        return C;
+        return result;
     }
 }
