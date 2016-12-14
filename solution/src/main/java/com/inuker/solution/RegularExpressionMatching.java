0687a3384d5795413d4b0da41d2efea74c5c138c
dingjikerbo <dingjikerbo@gmail.com>
2016-12-14 14:25:14 +0800

    fd

--- solution/src/main/java/com/inuker/solution/RegularExpressionMatching.java
+++ solution/src/main/java/com/inuker/solution/RegularExpressionMatching.java
@@ -18,33 +18,30 @@ public class RegularExpressionMatching {
 
     // 耗时153ms
     public boolean isMatch(String s, String p) {
-        // 如果p是空串，那么s必须也是空串
         if (p.length() == 0) {
             return s.length() == 0;
         }
 
-        // 如果p只有一个字符，那么s必须也是1个字符，而且两者要匹配，注意p不会是单独的*，因为*要跟在某个字符后面
         if (p.length() == 1) {
-            return s.length() == 1 && isMatch(s, 0, p);
+            return s.length() == 1 && isEquals(s, p);
         }
 
         if (p.charAt(1) != '*') {
-            if (s.length() < 1) {
-                return false;
-            }
-
-            return isMatch(s, 0, p) && isMatch(s.substring(1), p.substring(1));
+            return s.length() > 0 && isEquals(s, p) && isMatch(s.substring(1), p.substring(1));
         } else {
-            // 这里要注意判断s是否为空
-            if (s.length() > 0 && isMatch(s, 0, p)) {
-                return isMatch(s, p.substring(2)) || isMatch(s.substring(1), p);
+            if (s.length() > 0) {
+                if (isEquals(s, p)) {
+                    return isMatch(s, p.substring(2)) || isMatch(s.substring(1), p);
+                } else {
+                    return isMatch(s, p.substring(2));
+                }
             } else {
                 return isMatch(s, p.substring(2));
             }
         }
     }
 
-    private boolean isMatch(String s, int i, String p) {
-        return s.charAt(i) == p.charAt(0) || p.charAt(0) == '.';
+    private boolean isEquals(String s, String p) {
+        return s.charAt(0) == p.charAt(0) || p.charAt(0) == '.';
     }
 }
--- solution/src/main/java/com/inuker/solution/Solution.java
+++ solution/src/main/java/com/inuker/solution/Solution.java
@@ -16,6 +16,34 @@ import java.util.Queue;
 
 public class Solution {
 
+    public boolean isMatch(String s, String p) {
+        if (p.length() == 0) {
+            return s.length() == 0;
+        }
+
+        if (p.length() == 1) {
+            return s.length() == 1 && isEquals(s, p);
+        }
+
+        if (p.charAt(1) != '*') {
+            return s.length() > 0 && isEquals(s, p) && isMatch(s.substring(1), p.substring(1));
+        } else {
+            if (s.length() > 0) {
+                if (isEquals(s, p)) {
+                    return isMatch(s, p.substring(2)) || isMatch(s.substring(1), p);
+                } else {
+                    return isMatch(s, p.substring(2));
+                }
+            } else {
+                return isMatch(s, p.substring(2));
+            }
+        }
+    }
+
+    private boolean isEquals(String s, String p) {
+        return s.charAt(0) == p.charAt(0) || p.charAt(0) == '.';
+    }
+
     public int numDecodings(String s) {
         if (s.length() == 0) {
             return 0;
