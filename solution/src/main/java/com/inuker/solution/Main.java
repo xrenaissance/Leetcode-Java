c3b996a75c1ba5fdb5da76abdc67ab0b19e1f850
dingjikerbo <dingjikerbo@gmail.com>
2016-12-23 21:11:50 +0800

    fd

--- solution/src/main/java/com/inuker/solution/Main.java
+++ solution/src/main/java/com/inuker/solution/Main.java
@@ -13,7 +13,21 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-        int count = new NumberOfDigitOne().countDigitOne2(1410065408);
-        System.out.println(count);
+        Set<String> dict = new HashSet<String>(Arrays.asList(
+                        "si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar",
+                "ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or",
+                "rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh",
+                "co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an",
+                "me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"
+//                "hot","dot","dog","lot","log"
+        ));
+        List<List<String>> ladders = new WordLadderII().findLadders("qa", "sq", dict);
+//        List<List<String>> ladders = new WordLadderII().findLadders("hit", "cog", dict);
+        for (List<String> list : ladders) {
+            for (String word : list) {
+                System.out.print(word + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- solution/src/main/java/com/inuker/solution/WordLadderII.java
+++ solution/src/main/java/com/inuker/solution/WordLadderII.java
@@ -2,6 +2,7 @@ package com.inuker.solution;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -23,173 +24,119 @@ import java.util.Set;
  */
 public class WordLadderII {
 
-    private Map<String, List<String>> map;
-    private List<List<String>> results;
+    private int mMin = -1;
 
     public List<List<String>> findLadders(String start, String end, Set<String> dict) {
-        results = new ArrayList<List<String>>();
-        if (dict.size() == 0)
-            return results;
+        HashMap<String, Set<String>> map = new HashMap<String, Set<String>>();
+        bfs2(start, end, dict, map);
 
-        int min = Integer.MAX_VALUE;
+//        System.out.println(mMin);
+//        for (String key : map.keySet()) {
+//            System.out.print(key + ": ");
+//            for (String word : map.get(key)) {
+//                System.out.print(word + " ");
+//            }
+//            System.out.println();
+//        }
 
-        Queue<String> queue = new ArrayDeque<String>();
+        List<List<String>> result = new LinkedList<List<String>>();
+        List<String> path = new LinkedList<>(Arrays.asList(start));
+        dfs(start, end, map, result, path);
+        return result;
+    }
+
+    private void bfs2(String start, String end, Set<String> dict, HashMap<String, Set<String>> map) {
+        Queue<String> queue = new LinkedList<String>();
+        Queue<String> next = new LinkedList<String>();
+        int length = 1;
         queue.add(start);
-
-        map = new HashMap<String, List<String>>();
-
-        Map<String, Integer> ladder = new HashMap<String, Integer>();
-        for (String string : dict) {
-            ladder.put(string, Integer.MAX_VALUE);
-        }
-        ladder.put(start, 0);
-
         dict.add(end);
-        //BFS: Dijisktra search
         while (!queue.isEmpty()) {
-
             String word = queue.poll();
 
-            int step = ladder.get(word) + 1;//'step' indicates how many steps are needed to travel to one word.
+            Set<String> set = map.get(word);
+            if (set == null) {
+                set = new HashSet<String>();
+                map.put(word, set);
+            }
 
-            if (step > min) break;
-
+            StringBuilder sb = new StringBuilder(word);
             for (int i = 0; i < word.length(); i++) {
-                StringBuilder builder = new StringBuilder(word);
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    builder.setCharAt(i, ch);
-                    String new_word = builder.toString();
-                    if (ladder.containsKey(new_word)) {
+                char c = word.charAt(i);
+                for (int j = 0; j < 26; j++) {
+                    if (j + 'a' == c) {
+                        continue;
+                    }
+                    sb.setCharAt(i, (char) (j + 'a'));
+                    String t = sb.toString();
 
-                        if (step > ladder.get(new_word))//Check if it is the shortest path to one word.
-                            continue;
-                        else if (step < ladder.get(new_word)) {
-                            queue.add(new_word);
-                            ladder.put(new_word, step);
-                        } else ;// It is a KEY line. If one word already appeared in one ladder,
-                        // Do not insert the same word inside the queue twice. Otherwise it gets TLE.
+                    if (t.equals(end)) {
+                        mMin = length + 1;
+                    }
 
-                        if (map.containsKey(new_word)) //Build adjacent Graph
-                            map.get(new_word).add(word);
-                        else {
-                            List<String> list = new LinkedList<String>();
-                            list.add(word);
-                            map.put(new_word, list);
-                            //It is possible to write three lines in one:
-                            //map.put(new_word,new LinkedList<String>(Arrays.asList(new String[]{word})));
-                            //Which one is better?
-                        }
+                    if (dict.contains(t)) {
+                        set.add(t);
+                        next.add(t);
+                    }
+                }
+                sb.setCharAt(i, c);
+            }
 
-                        if (new_word.equals(end))
-                            min = step;
-
-                    }//End if dict contains new_word
-                }//End:Iteration from 'a' to 'z'
-            }//End:Iteration from the first to the last
-        }//End While
-
-        //BackTracking
-        LinkedList<String> result = new LinkedList<String>();
-        backTrace(end, start, result);
-
-        return results;
+            if (queue.isEmpty() && mMin == -1) {
+                queue.addAll(next);
+                next.clear();
+                length++;
+            }
+        }
     }
 
-    private void backTrace(String word, String start, List<String> list) {
-        if (word.equals(start)) {
-            list.add(0, start);
-            results.add(new ArrayList<String>(list));
-            list.remove(0);
+    private void dfs(String start, String end, Map<String, Set<String>> map, List<List<String>> result, List<String> path) {
+        if (start.equals(end)) {
+            result.add(new ArrayList<String>(path));
             return;
         }
-        list.add(0, word);
-        if (map.get(word) != null)
-            for (String s : map.get(word))
-                backTrace(s, start, list);
-        list.remove(0);
+
+        if (path.size() >= mMin) {
+            return;
+        }
+
+        Set<String> words = map.get(start);
+        for (String word : words) {
+            if (path.contains(word)) {
+                continue;
+            }
+            path.add(word);
+            dfs(word, end, map, result, path);
+            path.remove(path.size() - 1);
+        }
     }
 
-    /**
-     public List<List<String>> findLadders(String start, String end, Set<String> dict) {
-     List<List<String>> res = new ArrayList<List<String>>();
-     HashMap<String, ArrayList<String>> nodeNeighbors = new HashMap<String, ArrayList<String>>();// Neighbors for every node
-     HashMap<String, Integer> distance = new HashMap<String, Integer>();// Distance of every node from the start node
-     ArrayList<String> solution = new ArrayList<String>();
-
-     dict.add(end);
-     bfs(start, end, dict, nodeNeighbors, distance);
-     dfs(start, end, dict, nodeNeighbors, distance, solution, res);
-     return res;
-     }
-
-     // BFS: Trace every node's distance from the start node (level by level).
-     private void bfs(String start, String end, Set<String> dict, HashMap<String, ArrayList<String>> nodeNeighbors, HashMap<String, Integer> distance) {
-     for (String str : dict)
-     nodeNeighbors.put(str, new ArrayList<String>());
-
-     Queue<String> queue = new LinkedList<String>();
-     queue.offer(start);
-     distance.put(start, 0);
-
-     while (!queue.isEmpty()) {
-     int count = queue.size();
-     boolean foundEnd = false;
-     for (int i = 0; i < count; i++) {
-     String cur = queue.poll();
-     int curDistance = distance.get(cur);
-     ArrayList<String> neighbors = getNeighbors(cur, dict);
-
-     for (String neighbor : neighbors) {
-     nodeNeighbors.get(cur).add(neighbor);
-     if (!distance.containsKey(neighbor)) {// Check if visited
-     distance.put(neighbor, curDistance + 1);
-     if (end.equals(neighbor))// Found the shortest path
-     foundEnd = true;
-     else
-     queue.offer(neighbor);
-     }
-     }
-     }
-
-     if (foundEnd)
-     break;
-     }
-     }
-
-     // Find all next level nodes.
-     private ArrayList<String> getNeighbors(String node, Set<String> dict) {
-     ArrayList<String> res = new ArrayList<String>();
-     char chs[] = node.toCharArray();
-
-     for (char ch ='a'; ch <= 'z'; ch++) {
-     for (int i = 0; i < chs.length; i++) {
-     if (chs[i] == ch) continue;
-     char old_ch = chs[i];
-     chs[i] = ch;
-     if (dict.contains(String.valueOf(chs))) {
-     res.add(String.valueOf(chs));
-     }
-     chs[i] = old_ch;
-     }
-
-     }
-     return res;
-     }
-
-     // DFS: output all paths with the shortest distance.
-     private void dfs(String cur, String end, Set<String> dict, HashMap<String, ArrayList<String>> nodeNeighbors, HashMap<String, Integer> distance, ArrayList<String> solution, List<List<String>> res) {
-     solution.add(cur);
-     if (end.equals(cur)) {
-     res.add(new ArrayList<String>(solution));
-     }
-     else {
-     for (String next : nodeNeighbors.get(cur)) {
-     if (distance.get(next) == distance.get(cur) + 1) {
-     dfs(next, end, dict, nodeNeighbors, distance, solution, res);
-     }
-     }
-     }
-     solution.remove(solution.size() - 1);
-     }
-     */
+    //    private void bfs(String start, String end, Set<String> dict, HashMap<String, Set<String>> map) {
+//        Set<String> visited = new HashSet<String>();
+//        dict.add(start);
+//        dict.add(end);
+//        for (String word : dict) {
+//            Set<String> set = map.get(word);
+//            if (set == null) {
+//                set = new HashSet<String>();
+//                map.put(word, set);
+//            }
+//
+//            StringBuilder sb = new StringBuilder(word);
+//            for (int i = 0; i < word.length(); i++) {
+//                char c = word.charAt(i);
+//                for (int j = 0; j < 26; j++) {
+//                    if (j + 'a' == c) {
+//                        continue;
+//                    }
+//                    sb.setCharAt(i, (char) (j + 'a'));
+//                    String t = sb.toString();
+//                    if (dict.contains(t)) {
+//                        set.add(t);
+//                    }
+//                }
+//                sb.setCharAt(i, c);
+//            }
+//        }
+//    }
 }
