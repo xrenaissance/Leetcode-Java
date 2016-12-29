cbe12d5bddaa11aaf7dfab42a5f6da78905ba7c7
dingjikerbo <dingjikerbo@gmail.com>
2016-12-29 21:04:28 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/GeneralizedAbbreviation.java
@@ -0,0 +1,14 @@
+package com.inuker.solution;
+
+import java.util.List;
+
+/**
+ * Created by liwentian on 2016/12/29.
+ */
+
+public class GeneralizedAbbreviation {
+
+    public List<String> generateAbbreviations(String word) {
+
+    }
+}
