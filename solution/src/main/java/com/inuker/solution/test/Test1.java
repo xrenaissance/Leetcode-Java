fdddbcff363085c635332c0911fa43d459fddea8
liwentian <liwentian@xiaomi.com>
2017-01-08 22:52:40 +0800

    fd

--- solution/src/main/java/com/inuker/solution/test/Test1.java
+++ solution/src/main/java/com/inuker/solution/test/Test1.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 import java.util.TreeMap;
 
 import javax.swing.plaf.basic.BasicScrollPaneUI;
@@ -22,66 +23,53 @@ import javax.swing.plaf.basic.BasicScrollPaneUI;
 
 public class Test1 {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || p == null || q == null) {
-            return null;
-        }
-        HashMap<TreeNode, TreeNode> parents = new HashMap<>();
-        parents.put(root, null);
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            if (parents.containsKey(p) && parents.containsKey(q)) {
-                break;
-            }
-
-            TreeNode node = queue.poll();
-            if (node.left != null) {
-                queue.add(node.left);
-                parents.put(node.left, node);
-            }
-
-            if (node.right != null) {
-                queue.add(node.right);
-                parents.put(node.right, node);
+    // l, r, m
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.isEmpty() || root != null) {
+            if (root != null) {
+                result.add(0, root.val);
+                stack.push(root);
+                root = root.right;
+            } else {
+                root = stack.pop();
+                root = root.left;
             }
         }
-
-        if (!parents.containsKey(p) || !parents.containsKey(q)) {
-            return null;
-        }
-
-        Set<TreeNode> set = new HashSet<TreeNode>();
-        for (TreeNode node = p; node != null; node = parents.get(node)) {
-            set.add(node);
-        }
-
-        for (TreeNode node = q; node != null; node = parents.get(node)) {
-            if (!set.add(node)) {
-                return node;
-            }
-        }
-
-        return null;
+        return result;
     }
 
-    public String shortestPalindrome(String s) {
-        StringBuilder builder = new StringBuilder(s);
-        return builder.reverse().substring(0, s.length() - getCommonLength(s)) + s;
-    }
-
-    private int getCommonLength(String str) {
-        String rev = new StringBuilder(str).reverse().toString();
-        return getCommonLength(str + rev, str.length());
-    }
-
-    private int getCommonLength(String s, int max) {
-        for (int i = s.length() - max; i < s.length(); i++) {
-            if (s.startsWith(s.substring(i))) {
-                return s.length() - i;
+    // m, l, r
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.isEmpty() || root != null) {
+            if (root != null) {
+                result.add(root.val);
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                root = root.right;
             }
         }
-        return 0;
+        return result;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.isEmpty() || root != null) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                result.add(root.val);
+                root = root.right;
+            }
+        }
+        return result;
     }
 }
