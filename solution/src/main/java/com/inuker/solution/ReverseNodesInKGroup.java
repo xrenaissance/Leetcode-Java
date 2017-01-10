50dd7aeba4306fa6e5e132e8744bfef4cfb1ef10
dingjikerbo <dingjikerbo@gmail.com>
2017-01-10 16:57:35 +0800

    fd

--- solution/src/main/java/com/inuker/solution/ReverseNodesInKGroup.java
+++ solution/src/main/java/com/inuker/solution/ReverseNodesInKGroup.java
@@ -7,18 +7,11 @@ package com.inuker.solution;
 public class ReverseNodesInKGroup {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k == 1) {
-            return head;
-        }
         int size = 0;
-        ListNode dummy = new ListNode(0);
-        for (ListNode p = head; p != null; p = p.next, size++);
+        ListNode dummy = new ListNode(0), cur = dummy, p;
+        for (p = head; p != null; p = p.next, size++);
 
-        for (ListNode cur = dummy, p = head; p != null; ) {
-            if (size < k) {
-                cur.next = p;
-                break;
-            }
+        for (p = head; size >= k; size -= k) {
             ListNode tail = p;
             for (int i = 0; i < k; i++) {
                 ListNode next = p.next;
@@ -26,36 +19,9 @@ public class ReverseNodesInKGroup {
                 cur.next = p;
                 p = next;
             }
-            size -= k;
             cur = tail;
-            cur.next = null;
         }
-        return dummy.next;
-    }
-
-    public ListNode reverseKGroup2(ListNode head, int k) {
-        int count = 0;
-        for (ListNode p = head; p != null; p = p.next, count++);
-        ListNode dummy = new ListNode(0), prev = dummy, last = null;
-        for (ListNode p = head; p != null; ) {
-            if (count >= k) {
-                for (int i = 0; i < k; i++) {
-                    ListNode next = p.next;
-                    p.next = prev.next;
-                    prev.next = p;
-                    if (last == null) {
-                        last = p;
-                    }
-                    p = next;
-                }
-                count -= k;
-                prev = last;
-                last = null;
-            } else {
-                prev.next = p;
-                break;
-            }
-        }
+        cur.next = p;
         return dummy.next;
     }
 }
