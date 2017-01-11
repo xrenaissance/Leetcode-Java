8f7025cee10de6812f41f7968915f586eaec99f7
liwentian <liwentian@xiaomi.com>
2017-01-11 21:19:10 +0800

    fd

--- solution/src/main/java/com/inuker/solution/GraphValidTree.java
+++ solution/src/main/java/com/inuker/solution/GraphValidTree.java
@@ -7,7 +7,11 @@ import java.util.Arrays;
  */
 
 /**
- * 这题要问清楚是否会有重复的边，还有[0,1]和[1,0]也会认为是重复的
+ * 题目中已声明不会有重复的边，类似[0,1]和[1,0]认为是重复的，也不会同时存在
+ */
+
+/**
+ * 这题就是给了一堆边，看这些边构成的有向图会不会有环，另外这些边是不是都连在一起的
  */
 public class GraphValidTree {
 
@@ -23,8 +27,10 @@ public class GraphValidTree {
             int x = find(nums, edges[i][0]);
             int y = find(nums, edges[i][1]);
 
-            // if two vertices happen to be in the same set
-            // then there's a cycle
+            /**
+             * 既然题目中已经说明不会有重复的边，所以如果x==y说明x和y已经有一条路径相通了，
+             * 如果再多一条路径就要构成环了，所以这里直接return false
+             */
             if (x == y) return false;
 
             // union
