153d5b0a81267fd8d47b3999d13d091e8f1c8d64
dingjikerbo <dingjikerbo@gmail.com>
2017-03-13 10:12:49 +0800

    fd

--- solution/src/main/java/com/inuker/solution/ConvertSortedArrayToBinarySearchTree.java
+++ solution/src/main/java/com/inuker/solution/ConvertSortedArrayToBinarySearchTree.java
@@ -7,19 +7,19 @@ package com.inuker.solution;
 public class ConvertSortedArrayToBinarySearchTree {
 
     public TreeNode sortedArrayToBST(int[] nums) {
-        return sortedArrayToBST(nums, 0, nums.length - 1);
+        return sortedArrayToBST(nums, 0, nums.length);
     }
 
     private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
-        if (start > end) {
+        if (start >= end) {
             return null;
         }
-        if (start == end) {
+        if (end - start == 1) {
             return new TreeNode(nums[start]);
         }
-        int mid = start + ((end - start) >> 1);
+        int mid = start + (end - start) / 2;
         TreeNode root = new TreeNode(nums[mid]);
-        root.left = sortedArrayToBST(nums, start, mid - 1);
+        root.left = sortedArrayToBST(nums, start, mid);
         root.right = sortedArrayToBST(nums, mid + 1, end);
         return root;
     }
