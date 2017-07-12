16d17878ba5a491c01898f9b4310295d024938f5
liwentian <wentian.li@hobot.cc>
2017-07-13 07:53:28 +0800

    fd

--- solution/src/main/java/com/inuker/solution/prac2017/tree/BinaryTreeMaximumPathSum.java
+++ solution/src/main/java/com/inuker/solution/prac2017/tree/BinaryTreeMaximumPathSum.java
@@ -1,11 +1,15 @@
-package com.inuker.solution;
+package com.inuker.solution.prac2017.tree;
+
+import com.inuker.solution.TreeNode;
 
 /**
- * Created by dingjikerbo on 16/11/28.
+ * Created by liwentian on 17/7/13.
  */
 
 /**
- * 单边的或者双边的，或只包含根节点的
+ * 有节点为负，空节点返回0，0大于负，但是0其实是无效，所以空节点应该返回INT_MIN
+ * 最终返回的max中，要考虑各种可能性
+ * 算单边路径时，如果左右子树都小于0，就只算根节点
  */
 public class BinaryTreeMaximumPathSum {
 
@@ -13,23 +17,18 @@ public class BinaryTreeMaximumPathSum {
         return maxPathSum(root, null);
     }
 
-    /**
-     * max表示包含root的单边路径最大和
-     */
     private int maxPathSum(TreeNode root, int[] max) {
         if (root == null) {
-            return Integer.MIN_VALUE;  // 此处容易错
+            return Integer.MIN_VALUE;
         }
         int[] left = new int[1], right = new int[1];
         int leftMax = maxPathSum(root.left, left);
         int rightMax = maxPathSum(root.right, right);
         if (max != null) {
-            max[0] = max(left[0], right[0], 0) + root.val; // 此处容易错
+            max[0] = max(left[0], right[0], 0) + root.val;
         }
-
-        // 容易错，要考虑到所有可能的情况
-        return max(leftMax, rightMax, root.val, left[0] + right[0] + root.val,
-                left[0] + root.val, right[0] + root.val);
+        return max(leftMax, rightMax, left[0] + right[0] + root.val,
+                left[0] + root.val, right[0] + root.val, root.val);
     }
 
     private int max(int... vals) {
