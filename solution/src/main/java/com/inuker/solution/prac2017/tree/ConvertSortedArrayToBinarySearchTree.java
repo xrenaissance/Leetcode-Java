f3de38573e1b297db7b54148bd6d18810bd719bd
liwentian <wentian.li@hobot.cc>
2017-07-14 06:58:02 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/prac2017/tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,26 @@
+package com.inuker.solution.prac2017.tree;
+
+import com.inuker.solution.TreeNode;
+
+/**
+ * Created by liwentian on 17/7/14.
+ */
+
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, start, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, end);
+        return root;
+    }
+}
