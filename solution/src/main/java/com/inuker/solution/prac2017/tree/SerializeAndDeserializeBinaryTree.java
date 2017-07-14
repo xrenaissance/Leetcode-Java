7b8da7bfd09f991ae8ed81bbf873ea3d52e297db
liwentian <wentian.li@hobot.cc>
2017-07-14 08:59:29 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/prac2017/tree/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,19 @@
+package com.inuker.solution.prac2017.tree;
+
+import com.inuker.solution.TreeNode;
+
+/**
+ * Created by liwentian on 17/7/14.
+ */
+
+public class SerializeAndDeserializeBinaryTree {
+
+    public String serialize(TreeNode root) {
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
+}
