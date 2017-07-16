5f45b51d453b0eae93743dd55a113b3b2710683a
liwentian <wentian.li@hobot.cc>
2017-07-16 10:04:36 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/prac2017/tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,30 @@
+package com.inuker.solution.prac2017.tree;
+
+import com.inuker.solution.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by liwentian on 17/7/16.
+ */
+
+public class BinaryTreeInorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> list = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                list.add(root.val);
+                root = root.right;
+            }
+        }
+        return list;
+    }
+}
