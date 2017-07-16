3bc03fe9a9e6cf99b8085697ae7583ee7e1028ba
liwentian <wentian.li@hobot.cc>
2017-07-16 08:19:19 +0800

    fd

--- solution/src/main/java/com/inuker/solution/FlattenBinaryTreeToLinkedList.java
+++ solution/src/main/java/com/inuker/solution/FlattenBinaryTreeToLinkedList.java
@@ -1,12 +1,40 @@
 package com.inuker.solution;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Created by dingjikerbo on 16/11/30.
  */
 
 public class FlattenBinaryTreeToLinkedList {
 
+    /**
+     * 非递归，先序遍历一遍，再串起来
+     */
     public void flatten(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+
+        List<TreeNode> list = new ArrayList<>();
+
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                list.add(root);
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop().right;
+            }
+        }
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            list.get(i).left = null;
+            list.get(i).right = list.get(i + 1);
+        }
+    }
+
+    public void flatten2(TreeNode root) {
         helper(root);
     }
 
@@ -28,7 +56,7 @@ public class FlattenBinaryTreeToLinkedList {
             root.left = null;
         }
 
-        rightTail = helper(root.right);
+        rightTail = helper(right);
 
         return rightTail != null ? rightTail : leftTail;
     }
