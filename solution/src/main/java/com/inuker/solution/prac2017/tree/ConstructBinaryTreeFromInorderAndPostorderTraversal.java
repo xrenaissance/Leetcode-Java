b692294dead8e49b98a39f9a6fa2a0b6e9c18612
dingjikerbo <dingjikerbo@gmail.com>
2017-07-19 07:22:59 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/prac2017/tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,36 @@
+package com.inuker.solution.prac2017.tree;
+
+import com.inuker.solution.TreeNode;
+
+/**
+ * Created by dingjikerbo on 2017/7/19.
+ */
+
+public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder == null || inorder.length == 0) {
+            return null;
+        }
+        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postEnd]);
+
+        int idx = inStart;
+        for ( ; idx <= inEnd; idx++) {
+            if (inorder[idx] == postorder[postEnd]) {
+                break;
+            }
+        }
+
+        int len = idx - inStart;
+        root.left = buildTree(inorder, inStart, idx - 1, postorder, postStart, postStart + len - 1);
+        root.right = buildTree(inorder, idx + 1, inEnd, postorder, postStart + len, postEnd - 1);
+        return root;
+    }
+}
