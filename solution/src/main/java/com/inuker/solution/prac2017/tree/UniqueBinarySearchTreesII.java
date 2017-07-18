50d73a15385c348232cb36b59a9b342175aa629b
dingjikerbo <dingjikerbo@gmail.com>
2017-07-19 07:43:23 +0800

    fd

--- solution/src/main/java/com/inuker/solution/prac2017/tree/UniqueBinarySearchTreesII.java
+++ solution/src/main/java/com/inuker/solution/prac2017/tree/UniqueBinarySearchTreesII.java
@@ -10,7 +10,12 @@ import java.util.List;
 
 public class UniqueBinarySearchTreesII {
 
+
     public List<TreeNode> generateTrees(int n) {
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
 
     }
 }
