0c4baf056c109b28f27df14beb8ceb9f5321d2de
liwentian <wentian.li@hobot.cc>
2017-07-24 09:46:58 +0800

    fd

--- solution/src/main/java/com/inuker/solution/BinaryTreeLongestConsecutiveSequence.java
+++ solution/src/main/java/com/inuker/solution/BinaryTreeLongestConsecutiveSequence.java
@@ -4,22 +4,65 @@ package com.inuker.solution;
  * Created by dingjikerbo on 2016/12/2.
  */
 
+/**
+ * https://leetcode.com/articles/binary-tree-longest-consecutive-sequence/
+ */
 public class BinaryTreeLongestConsecutiveSequence {
 
-    private int max = 0;
+    private int longest;
 
     public int longestConsecutive(TreeNode root) {
-        if(root == null) return 0;
-        helper(root, 0, root.val);
-        return max;
+        if (root == null) {
+            return 0;
+        }
+        longestConsecutive(root, 0, root.val);
+        return longest;
     }
 
-    public void helper(TreeNode root, int cur, int target){
-        if(root == null) return;
-        if(root.val == target) cur++;
-        else cur = 1;
-        max = Math.max(cur, max);
-        helper(root.left, cur, root.val + 1);
-        helper(root.right, cur, root.val + 1);
+    public void longestConsecutive(TreeNode root, int length, int target) {
+        if (root == null) {
+            return;
+        } else if (root.val == target) {
+            length++;
+        } else {
+            length = 1;
+        }
+        longest = Math.max(longest, length);
+        longestConsecutive(root.left, length, root.val + 1);
+        longestConsecutive(root.right, length, root.val + 1);
     }
+
+    /** 这种写法很直观，不过不够简洁
+    private int longest = 1;
+
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        longestConsecutive(root, 1);
+        return longest;
+    }
+
+    public void longestConsecutive(TreeNode root, int length) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            if (root.left.val == root.val + 1) {
+                longest = Math.max(longest, length + 1);
+                longestConsecutive(root.left, length + 1);
+            } else {
+                longestConsecutive(root.left, 1);
+            }
+        }
+
+        if (root.right != null) {
+            if (root.right.val == root.val + 1) {
+                longest = Math.max(longest, length + 1);
+                longestConsecutive(root.right, length + 1);
+            } else {
+                longestConsecutive(root.right, 1);
+            }
+        }
+    }*/
 }
