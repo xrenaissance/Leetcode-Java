e6b56a29989084e1c96b8e122c5368a830ac43a1
liwentian <wentian.li@hobot.cc>
2017-08-09 10:24:14 +0800

    fd

--- solution/src/main/java/com/inuker/solution/Main.java
+++ solution/src/main/java/com/inuker/solution/Main.java
@@ -5,10 +5,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode root = TreeNode.buildTree(new Integer[] {
-                3, 1, null,
-        });
-        int max = new com.inuker.solution.prac2017.tree.LargestBSTSubtree().largestBSTSubtree(root);
-        System.out.println(max);
+        List<Integer> list = new com.inuker.solution.amazon.FindAllAnagramsInAString().findAnagrams("cbaebabacd", "abc");
+        for (Integer n : list) {
+            System.out.println(n);
+        }
     }
 }
