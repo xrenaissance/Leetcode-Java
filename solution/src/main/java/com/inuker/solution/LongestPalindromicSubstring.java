09d29770136914fafc8041b58412a296e2401c2f
liwentian <wentian.li@hobot.cc>
2017-08-10 08:01:04 +0800

    longest palindrom

--- solution/src/main/java/com/inuker/solution/LongestPalindromicSubstring.java
+++ solution/src/main/java/com/inuker/solution/LongestPalindromicSubstring.java
@@ -4,64 +4,57 @@ package com.inuker.solution;
  * Created by dingjikerbo on 2016/11/16.
  */
 
+/**
+ * 这题采用动态规划也可，但是不论时间还是空间性能都不好
+ */
 public class LongestPalindromicSubstring {
 
-    // 耗时78ms，彻底的O(n^2)
+    private int begin, maxLen;
+
+    // 耗时14ms，平均复杂度O(n)
     public String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            helper(s, i, i);
+            helper(s, i, i + 1);
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    private void helper(String s, int i, int j) {
+        for (; i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j); i--, j++) ;
+        int len = j - i - 1;
+        if (len > maxLen) {
+            maxLen = len;
+            begin = i + 1;
+        }
+    }
+
+    // 动态规划，耗时73ms，复杂度O(n^2)
+    public String longestPalindrome2(String s) {
         int len = s.length();
 
         if (len == 0) {
-            return "";
+            return s;
         }
 
         boolean[][] f = new boolean[len][len];
 
-        f[0][0] = true;
+        int index = 0;
+        int max = 1;
 
-        int maxLen = 1, begin = 0;
-
-        char[] ss = s.toCharArray();
-        for (int i = 1; i < len; i++) {
-            for (int j = 0; j < i; j++) {
-                // 这里注意不能掉了j >= i - 2
-                if ((j >= i - 2 || f[j + 1][i - 1]) && ss[j] == ss[i]) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(j) == s.charAt(i) && (i - j < 2 || f[j + 1][i - 1])) {
                     f[j][i] = true;
 
-                    if (i - j + 1 > maxLen) {
-                        maxLen = i - j + 1;
-                        begin = j;
+                    if (i - j + 1 > max) {
+                        max = i - j + 1;
+                        index = j;
                     }
                 }
             }
         }
 
-        return s.substring(begin, begin + maxLen);
-    }
-
-    private int begin, maxLen;
-
-    // 耗时14ms，最坏情况O(n^2)，平均情况O(n*len)
-    public String longestPalindrome2(String s) {
-        int len = s.length();
-
-        if (len == 0) {
-            return "";
-        }
-
-        char[] ss = s.toCharArray();
-        for (int i = 0; i < len; i++) {
-            findLongestPalindrome(ss, i, i);
-            findLongestPalindrome(ss, i, i + 1);
-        }
-
-        return s.substring(begin, begin + maxLen);
-    }
-
-    private void findLongestPalindrome(char[] s, int j, int k) {
-        for ( ; j >= 0 && k < s.length && s[j] == s[k]; j--, k++);
-        if (k - j - 1 > maxLen) {
-            maxLen = k - j - 1;
-            begin = j + 1;
-        }
+        return s.substring(index, index + max);
     }
 }
