4110d34fa73c0db7d6c1de20c1a6f990eea5c10a
liwentian <wentian.li@hobot.cc>
2017-08-15 20:50:34 +0800

    combination sum

--- solution/src/main/java/com/inuker/solution/snapchat/CombinationSumII.java
+++ solution/src/main/java/com/inuker/solution/snapchat/CombinationSumII.java
@@ -1,16 +1,15 @@
-package com.inuker.solution;
+package com.inuker.solution.snapchat;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by dingjikerbo on 16/11/22.
+ * Created by liwentian on 17/8/15.
  */
 
 public class CombinationSumII {
 
-    // 耗时29ms
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new LinkedList<>();
         Arrays.sort(candidates);
@@ -36,35 +35,4 @@ public class CombinationSumII {
 
         helper(candidates, result, list, target, index + 1);
     }
-
-    /**
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new LinkedList<List<Integer>>();
-        Arrays.sort(candidates);
-        dfs(candidates, 0, target, result, new LinkedList<Integer>());
-        return result;
-    }
-
-    private void dfs(int[] candidates, int start, int target, List<List<Integer>> result, List<Integer> path) {
-        if (target < 0) {
-            return;
-        }
-
-        if (target == 0) {
-            result.add(new LinkedList<Integer>(path));
-            return;
-        }
-
-        for (int i = start; i < candidates.length; i++) {
-            if (i > start && candidates[i] == candidates[i - 1]) {
-                continue;
-            }
-
-            path.add(candidates[i]);
-
-            // 关键是这里变成i + 1
-            dfs(candidates, i + 1, target - candidates[i], result, path);
-            path.remove(path.size() - 1);
-        }
-    }*/
 }
