28a77ce47b790f4325d84e93552ddcfac7b2a447
liwentian <wentian.li@hobot.cc>
2017-08-17 21:50:27 +0800

    fd

--- solution/src/main/java/com/inuker/solution/SparseMatrixMultiplication.java
+++ solution/src/main/java/com/inuker/solution/SparseMatrixMultiplication.java
@@ -5,21 +5,9 @@ package com.inuker.solution;
  */
 
 /**
- * 这题有点意思，方法一对稀疏矩阵会超时，只要给里层两个for循环交换一下顺序即可
+ * 这题有点意思，只要给里层两个for循环交换一下顺序即可
  */
 public class SparseMatrixMultiplication {
-
-    public int[][] multiply(int[][] A, int[][] B) {
-        int[][] result = new int[A.length][B[0].length];
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B[0].length; j++) {
-                for (int k = 0; k < A[0].length; k++) {
-                    result[i][j] += A[i][k] * B[k][j];
-                }
-            }
-        }
-        return result;
-    }
 
     public int[][] multiply2(int[][] A, int[][] B) {
         int[][] result = new int[A.length][B[0].length];
