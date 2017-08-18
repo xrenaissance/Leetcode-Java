276633d230600c88cf72ddd174809e3f097b3e76
liwentian <wentian.li@hobot.cc>
2017-08-18 15:25:09 +0800

    f

---
+++ solution/src/main/java/com/inuker/solution/company/facebook/IntegerToEnglishWords.java
@@ -0,0 +1,25 @@
+package com.inuker.solution.company.facebook;
+
+/**
+ * Created by liwentian on 17/8/18.
+ */
+
+public class IntegerToEnglishWords {
+
+    private final String[] LESS_20 = {
+            "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+            "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    private final String[] LESS_100 = {
+            "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+    private final String[] THOUSANDS = {
+            "", "Thousand", "Million", "Billion"
+    };
+
+    public String numberToWords(int num) {
+        
+    }
+}
