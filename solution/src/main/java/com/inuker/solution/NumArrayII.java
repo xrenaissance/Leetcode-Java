0bd007c95951ca6f3cd6cd24d06f3b1c5753cb80
liwentian <wentian.li@hobot.cc>
2017-08-25 22:31:33 +0800

    fd

--- solution/src/main/java/com/inuker/solution/NumArrayII.java
+++ solution/src/main/java/com/inuker/solution/NumArrayII.java
@@ -3,16 +3,60 @@ package com.inuker.solution;
 /**
  * Created by dingjikerbo on 16/11/26.
  */
-
 /**
+ * 这题可以用segment tree或binary index tree(bit)，两者时间复杂度都是lgn，不过segment tree因为用到树，
+ * 而bit用的是数组，所以bit更省空间。
+ *
  * 关于BIT，可以参考http://www.geeksforgeeks.org/binary-indexed-tree-or-fenwick-tree-2/
  * 关于Segment Tree，可以参考 https://discuss.leetcode.com/topic/29918/17-ms-java-solution-with-segment-tree
+ *
  * https://leetcode.com/articles/range-sum-query-mutable/
- * https://leetcode.com/articles/recursive-approach-segment-trees-range-sum-queries-lazy-propagation/
- * 这题最直观的解法就是Segment Tree递归算法，可以重点关注
  */
 public class NumArrayII {
 
+    private int[] bit;
+
+    private int[] nums;
+
+    public NumArrayII(int[] nums) {
+        this.nums = nums;
+
+        bit = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            init(i, nums[i]);
+        }
+    }
+
+    public void update(int i, int val) {
+        int diff = val - nums[i];
+        nums[i] = val;
+        init(i, diff);
+    }
+
+    private void init(int i, int val) {
+        i++;
+        while (i < bit.length) {
+            bit[i] += val;
+            i += i & (-i);
+        }
+    }
+
+    private int getSum(int i) {
+        i++;
+        int sum = 0;
+        while (i > 0) {
+            sum += bit[i];
+            i -= i & (-i);
+        }
+        return sum;
+    }
+
+    public int sumRange(int i, int j) {
+        return getSum(j) - getSum(i - 1);
+    }
+
+    /**
     private SegmentTreeNode mRoot;
 
     public NumArrayII(int[] nums) {
@@ -75,7 +119,7 @@ public class NumArrayII {
         } else {
             return sumRange(root.left, i, mid) + sumRange(root.right, mid + 1, j);
         }
-    }
+    }*/
 }
 
 // Your NumArray object will be instantiated and called as such:
--- solution/src/main/java/com/inuker/solution/topics/BIT/NumArrayII.java
+++ solution/src/main/java/com/inuker/solution/topics/BIT/NumArrayII.java
@@ -4,79 +4,48 @@ package com.inuker.solution.topics.BIT;
  * Created by dingjikerbo on 16/11/26.
  */
 
-import com.inuker.solution.SegmentTreeNode;
-
-/**
- * 关于BIT，可以参考http://www.geeksforgeeks.org/binary-indexed-tree-or-fenwick-tree-2/
- * 关于Segment Tree，可以参考 https://discuss.leetcode.com/topic/29918/17-ms-java-solution-with-segment-tree
- * https://leetcode.com/articles/range-sum-query-mutable/
- * https://leetcode.com/articles/recursive-approach-segment-trees-range-sum-queries-lazy-propagation/
- * 这题最直观的解法就是Segment Tree递归算法，可以重点关注
- */
 public class NumArrayII {
 
-    private SegmentTreeNode mRoot;
+    private int[] bit;
+
+    private int[] nums;
 
     public NumArrayII(int[] nums) {
-        mRoot = buildTree(nums, 0, nums.length - 1);
-    }
+        this.nums = nums;
 
-    private SegmentTreeNode buildTree(int[] nums, int start, int end) {
-        if (start > end) {
-            return null;
+        bit = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            init(i, nums[i]);
         }
-        SegmentTreeNode root = new SegmentTreeNode(start, end);
-        if (start == end) {
-            root.sum = nums[start];
-            return root;
-        }
-        int mid = start + ((end - start) >>> 1);
-        root.left = buildTree(nums, start, mid);
-        root.right = buildTree(nums, mid + 1, end);
-        root.sum = root.left.sum + root.right.sum;
-        return root;
     }
 
     public void update(int i, int val) {
-        update(mRoot, i, val);
+        int diff = val - nums[i];
+        nums[i] = val;
+        init(i, diff);
     }
 
-    private void update(SegmentTreeNode root, int i, int val) {
-        int start = root.start, end = root.end;
+    private void init(int i, int val) {
+        i++;
+        while (i < bit.length) {
+            bit[i] += val;
+            i += i & (-i);
+        }
+    }
 
-        if (start == end) {
-            root.sum = val;
-            return;
+    private int getSum(int i) {
+        i++;
+        int sum = 0;
+        while (i > 0) {
+            sum += bit[i];
+            i -= i & (-i);
         }
-        int mid = start + ((end - start) >>> 1);
-        if (i <= mid) {
-            update(root.left, i, val);
-        } else {
-            update(root.right, i, val);
-        }
-        root.sum = root.left.sum + root.right.sum;
+        return sum;
     }
 
     public int sumRange(int i, int j) {
-        return sumRange(mRoot, i, j);
-    }
-
-    public int sumRange(SegmentTreeNode root, int i, int j) {
-        int start = root.start, end = root.end;
-
-        if (start == i && end == j) {
-            return root.sum;
-        }
-
-        int mid = start + ((end - start) >>> 1);
-
-        if (j <= mid) {
-            return sumRange(root.left, i, j);
-        } else if (i > mid) {
-            return sumRange(root.right, i, j);
-        } else {
-            return sumRange(root.left, i, mid) + sumRange(root.right, mid + 1, j);
-        }
+       return getSum(j) - getSum(i - 1);
     }
 }
 
