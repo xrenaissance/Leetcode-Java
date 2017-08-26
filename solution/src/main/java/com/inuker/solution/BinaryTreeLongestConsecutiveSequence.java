8153c7109ada4ad5654353869897e96ed4092794
liwentian <wentian.li@hobot.cc>
2017-08-26 15:08:18 +0800

    fd

--- solution/src/main/java/com/inuker/solution/BinaryTreeLongestConsecutiveSequence.java
+++ solution/src/main/java/com/inuker/solution/BinaryTreeLongestConsecutiveSequence.java
@@ -4,6 +4,8 @@ package com.inuker.solution;
  * Created by dingjikerbo on 2016/12/2.
  */
 
+import com.leetcode.library.TreeNode;
+
 /**
  * https://leetcode.com/articles/binary-tree-longest-consecutive-sequence/
  */
@@ -32,37 +34,5 @@ public class BinaryTreeLongestConsecutiveSequence {
         longestConsecutive(root.right, length, root.val + 1);
     }
 
-    /** 这种写法很直观，不过不够简洁
-    private int longest = 1;
 
-    public int longestConsecutive(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        longestConsecutive(root, 1);
-        return longest;
-    }
-
-    public void longestConsecutive(TreeNode root, int length) {
-        if (root == null) {
-            return;
-        }
-        if (root.left != null) {
-            if (root.left.val == root.val + 1) {
-                longest = Math.max(longest, length + 1);
-                longestConsecutive(root.left, length + 1);
-            } else {
-                longestConsecutive(root.left, 1);
-            }
-        }
-
-        if (root.right != null) {
-            if (root.right.val == root.val + 1) {
-                longest = Math.max(longest, length + 1);
-                longestConsecutive(root.right, length + 1);
-            } else {
-                longestConsecutive(root.right, 1);
-            }
-        }
-    }*/
 }
