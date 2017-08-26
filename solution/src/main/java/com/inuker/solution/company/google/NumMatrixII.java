a37670ffa91a7cbdc130301f03923c81288d5c27
liwentian <wentian.li@hobot.cc>
2017-08-26 09:55:11 +0800

    f

--- solution/src/main/java/com/inuker/solution/company/google/NumMatrixII.java
+++ solution/src/main/java/com/inuker/solution/company/google/NumMatrixII.java
@@ -1,15 +1,9 @@
-package com.inuker.solution;
+package com.inuker.solution.company.google;
 
 /**
- * Created by dingjikerbo on 16/11/27.
+ * Created by liwentian on 17/8/26.
  */
 
-/**
- * 这道题有两种方法
- * 1，2D BIT
- * 2，按行扫描，每一行都保存该行从左往右的和
- * 参考https://discuss.leetcode.com/topic/30343/java-2d-binary-indexed-tree-solution-clean-and-short-17ms
- */
 public class NumMatrixII {
 
     private int m, n;
@@ -64,40 +58,4 @@ public class NumMatrixII {
         }
         return sum(row2, col2) - sum(row2, col1 - 1) - sum(row1 - 1, col2) + sum(row1 - 1, col1 - 1);
     }
-
-    /**
-    private int[][] mMatrix;
-    private int mRow, mCol;
-
-    public NumMatrixII(int[][] matrix) {
-        if (matrix.length == 0) {
-            return;
-        }
-
-        mRow = matrix.length;
-        mCol = matrix[0].length;
-
-        for (int i = 0; i < mRow; i++) {
-            for (int j = 0; j < mCol; j++) {
-                matrix[i][j] += matrix[i][j - 1];
-            }
-        }
-        mMatrix = matrix;
-    }
-
-    public void update(int row, int col, int val) {
-        int orig = mMatrix[row][col] - (col > 0 ? mMatrix[row][col - 1] : 0);
-        int delta = val - orig;
-        for (int i = col; i < mCol; i++) {
-            mMatrix[row][i] += delta;
-        }
-    }
-
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for (int i = row1; i <= row2; i++) {
-            sum += mMatrix[i][col2] - (col1 > 0 ? mMatrix[i][col1 - 1] : 0);
-        }
-        return sum;
-    }*/
 }
