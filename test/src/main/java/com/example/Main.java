8082ce06935da2fce1f3852993e5c838c950ab01
liwentian <wentian.li@hobot.cc>
2017-08-31 10:12:56 +0800

    fd

--- test/src/main/java/com/example/Main.java
+++ test/src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ import com.leetcode.google.DecodeString;
 import com.leetcode.google.MissingRanges;
 import com.leetcode.google.SentenceScreenFitting;
 import com.leetcode.google.ShortestDistanceFromAllBuildings;
+import com.leetcode.google.StrobogrammaticNumberII;
 import com.leetcode.google.UTFValidation;
 import com.leetcode.google.WordSquares;
 import com.leetcode.google.ZigzagIterator;
@@ -15,10 +16,10 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        boolean n = new UTFValidation().validUtf8(new int[] {
-                197, 130, 1
-        });
-        System.out.println(n);
+        List<String> list = new StrobogrammaticNumberII().findStrobogrammatic(4);
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
     }
 }
 
