64a0e8cd214bdaf77d504ed48270e2aa78113b52
liwentian <wentian.li@hobot.cc>
2017-09-06 09:53:03 +0800

    fd

--- solution/src/main/java/com/inuker/solution/PalindromePermutationII.java
+++ solution/src/main/java/com/inuker/solution/PalindromePermutationII.java
@@ -7,30 +7,52 @@ import java.util.List;
  * Created by dingjikerbo on 2016/12/19.
  */
 
+/**
+ * https://leetcode.com/articles/palindrome-permutation-ii/
+ */
 public class PalindromePermutationII {
     /**
      * 这题就是生成一半的所有排列，然后镜像
      */
-    public List<String> generatePalindromes(String s) {
+    public static List<String> generatePalindromes(String s) {
         int[] counts = new int[256];
         for (char c : s.toCharArray()) {
             counts[c]++;
         }
+        List<String> list = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
-        StringBuilder single = new StringBuilder();
+        char single = 0;
         for (int i = 0; i < counts.length; i++) {
-            int count = counts[i];
-            for ( ; count > 1; sb.append((char) i), count -= 2);
-            if (count == 1) {
-                single.append((char) i);
+            if (counts[i] % 2 != 0) {
+                if (single != 0) {
+                    return list;
+                }
+                single = (char) i;
+                counts[i] = ((counts[i] >> 1) << 1);
+            }
+            for (int j = 0; j < counts[i]; j += 2) {
+                sb.append((char)i);
             }
         }
-        List<String> result = new LinkedList<String>();
-        helper(sb.toString(), single.toString(), result);
-        return result;
+
+        helper(sb, "" + (single != 0 ? single : ""), list);
+        return list;
     }
 
-    private void helper(String s, String t, List<String> list) {
+    private static void helper(StringBuilder sb, String cur, List<String> list) {
+        if (sb.length() == 0) {
+            list.add(cur);
+            return;
+        }
 
+        for (int i = 0; i < sb.length(); i++) {
+            if (i > 0 && sb.charAt(i) == sb.charAt(i - 1)) {
+                continue;
+            }
+            char c = sb.charAt(i);
+            sb.deleteCharAt(i);
+            helper(sb, c + cur + c, list);
+            sb.insert(i, c);
+        }
     }
 }
--- test/src/main/java/com/example/Main.java
+++ test/src/main/java/com/example/Main.java
@@ -16,6 +16,7 @@ import com.leetcode.google.ZigzagIterator;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -26,9 +27,51 @@ import java.util.function.Function;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> list = new RestoreIPAddresses().restoreIpAddresses("010010");
+        List<String> list = generatePalindromes("a");
         for (String s : list) {
-            System.out.println(s);
+            System.out.println("(" + s+")");
+        }
+    }
+
+    public static List<String> generatePalindromes(String s) {
+        int[] counts = new int[256];
+        for (char c : s.toCharArray()) {
+            counts[c]++;
+        }
+        List<String> list = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        char single = 0;
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] % 2 != 0) {
+                if (single != 0) {
+                    return list;
+                }
+                single = (char) i;
+                counts[i] = ((counts[i] >> 1) << 1);
+            }
+            for (int j = 0; j < counts[i]; j += 2) {
+                sb.append((char)i);
+            }
+        }
+
+        helper(sb, "" + (single != 0 ? single : ""), list);
+        return list;
+    }
+
+    private static void helper(StringBuilder sb, String cur, List<String> list) {
+        if (sb.length() == 0) {
+            list.add(cur);
+            return;
+        }
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (i > 0 && sb.charAt(i) == sb.charAt(i - 1)) {
+                continue;
+            }
+            char c = sb.charAt(i);
+            sb.deleteCharAt(i);
+            helper(sb, c + cur + c, list);
+            sb.insert(i, c);
         }
     }
 }
