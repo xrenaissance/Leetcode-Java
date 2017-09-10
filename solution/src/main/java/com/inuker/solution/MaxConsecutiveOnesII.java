ef67a89cb068c9aca3c0519614f6bc48e4fc422f
liwentian <wentian.li@hobot.cc>
2017-09-10 19:20:12 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/MaxConsecutiveOnesII.java
@@ -0,0 +1,38 @@
+package com.inuker.solution;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by liwentian on 2017/9/10.
+ */
+
+public class MaxConsecutiveOnesII {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, zero = 0, k = 1; // flip at most k zero
+        for (int l = 0, h = 0; h < nums.length; h++) {
+            if (nums[h] == 0)
+                zero++;
+            while (zero > k)
+                if (nums[l++] == 0)
+                    zero--;
+            max = Math.max(max, h - l + 1);
+        }
+        return max;
+    }
+
+    // for follow up
+    public int findMaxConsecutiveOnes2(int[] nums) {
+        int max = 0, k = 1; // flip at most k zero
+        Queue<Integer> zeroIndex = new LinkedList<>();
+        for (int l = 0, h = 0; h < nums.length; h++) {
+            if (nums[h] == 0)
+                zeroIndex.offer(h);
+            if (zeroIndex.size() > k)
+                l = zeroIndex.poll() + 1;
+            max = Math.max(max, h - l + 1);
+        }
+        return max;
+    }
+}
