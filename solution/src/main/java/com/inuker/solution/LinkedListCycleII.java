591118c050d704a3d6fb559c773a9ce06aaa2c22
liwentian <wentian.li@hobot.cc>
2017-09-11 21:43:29 +0800

    fd

---
+++ solution/src/main/java/com/inuker/solution/LinkedListCycleII.java
@@ -0,0 +1,35 @@
+package com.inuker.solution;
+
+/**
+ * Created by liwentian on 2017/9/11.
+ */
+
+public class LinkedListCycleII {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+
+        ListNode firstp = head;
+        ListNode secondp = head;
+        boolean isCycle = false;
+
+        while (firstp != null && secondp != null) {
+            firstp = firstp.next;
+            if (secondp.next == null) return null;
+            secondp = secondp.next.next;
+            if (firstp == secondp) {
+                isCycle = true;
+                break;
+            }
+        }
+
+        if (!isCycle) return null;
+        firstp = head;
+        while (firstp != secondp) {
+            firstp = firstp.next;
+            secondp = secondp.next;
+        }
+
+        return firstp;
+    }
+}
