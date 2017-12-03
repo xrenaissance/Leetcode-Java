86ad19e786cbccbf61eabc9a9937f4dacdb24044
dingjikerbo <dingjikerbo@gmail.com>
2017-12-03 22:04:53 +0800

    fd

--- solution/src/main/java/com/inuker/solution/SlidingWindowMaximum.java
+++ solution/src/main/java/com/inuker/solution/SlidingWindowMaximum.java
@@ -22,62 +22,42 @@ public class SlidingWindowMaximum {
      * 注意PriorityQueue的remove复杂度是O(k)，所以本题复杂度是O(n*k)
      * 可以将PriorityQueue转成TreeMap，复杂度为O(n*lgk)
      */
-    // 耗时58ms
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums.length == 0) {
+        if (nums.length == 0 || k == 0) {
             return new int[0];
         }
-
-        Queue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer i1, Integer i2) {
-                return i2 - i1;
-            }
-        });
-
-        for (int i = 0; i < k; i++) {
-            queue.offer(nums[i]);
-        }
-
         int[] result = new int[nums.length - k + 1];
-        result[0] = queue.peek();
-
-        for (int i = 1; i < result.length; i++) {
-            queue.remove(nums[i - 1]);
-            queue.offer(nums[i + k - 1]);
-            result[i] = queue.peek();
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.<Integer>reverseOrder());
+        for (int i = 0; i < nums.length; i++) {
+            queue.offer(nums[i]);
+            if (queue.size() > k) {
+                queue.remove(nums[i - k]);
+            }
+            if (i - k + 1 >= 0) {
+                result[i - k + 1] = queue.peek();
+            }
         }
-
         return result;
     }
 
-    // 耗时23ms
-    // queue中存的是索引
     public int[] maxSlidingWindow2(int[] nums, int k) {
-        if (nums.length == 0) {
+        if (nums.length == 0 || k == 0) {
             return new int[0];
         }
-
-        Deque<Integer> queue = new LinkedList<Integer>();
-
         int[] result = new int[nums.length - k + 1];
-
+        Deque<Integer> queue = new LinkedList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (!queue.isEmpty() && queue.peek() <= i - k) {
-                queue.removeFirst();
-            }
-
-            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+            while (!queue.isEmpty() && nums[i] > nums[queue.peekLast()]) {
                 queue.pollLast();
             }
-
             queue.offerLast(i);
-
-            if (i >= k - 1) {
-                result[i - k + 1] = nums[queue.peek()];
+            if (queue.peekFirst() + k <= i) {
+                queue.pollFirst();
+            }
+            if (i - k + 1 >= 0) {
+                result[i - k + 1] = nums[queue.peekFirst()];
             }
         }
-
         return result;
     }
 }
--- test/src/main/java/com/inuker/test/main.java
+++ test/src/main/java/com/inuker/test/main.java
@@ -13,39 +13,34 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 
 public class main {
 
     public static void main(String[] args) {
-        List<String> list = summaryRanges(new int[] {
-                0, 1, 2, 4, 5, 7
-        });
-        for (String s : list) {
-            System.out.print(s + " ");
-        }
     }
 
-    public static List<String> summaryRanges(int[] nums) {
-        List<String> list = new LinkedList<>();
-        if (nums == null || nums.length == 0) {
-            return list;
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) {
+            return new int[0];
         }
-        int start = nums[0], to = start;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == to + 1) {
-                to++;
-            } else {
-                list.add(getRange(start, to));
-                start = to = nums[i];
+        int[] result = new int[nums.length - k + 1];
+        Deque<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!queue.isEmpty() && nums[i] > nums[queue.peekLast()]) {
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+            if (queue.peekFirst() + k <= i) {
+                queue.pollFirst();
+            }
+            if (i - k + 1 >= 0) {
+                result[i - k + 1] = nums[queue.peekFirst()];
             }
         }
-        list.add(getRange(start, to));
-        return list;
-    }
-
-    private static String getRange(int start, int to) {
-        return to > start ? start + "->" + to : String.valueOf(to);
+        return result;
     }
 }
