bb6bc600abe0ab920f7bbe61098b2c3c82f57abd
dingjikerbo <dingjikerbo@gmail.com>
2017-12-05 09:43:38 +0800

    fd

--- solution/src/main/java/com/inuker/solution/LongestSubstringWithAtMostKDistinctCharacters.java
+++ solution/src/main/java/com/inuker/solution/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -11,29 +11,20 @@ public class LongestSubstringWithAtMostKDistinctCharacters {
     /**
      * 思路跟Longest Substring With At Most Two Distinct Characters一样，只是给2改成k，要注意k等于0时返回0
      */
-    // 耗时9ms
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if (k == 0) {
-            return 0;
-        }
-
-        int[] count = new int[256];
-        int distinct = 0, longest = 0;
-
-        for (int i = 0, j = 0; j < s.length(); j++) {
-            if (count[s.charAt(j)]++ == 0) {
-                distinct++;
-            }
-
-            for ( ; i < j && distinct > k; ) {
-                if (--count[s.charAt(i++)] == 0) {
-                    --distinct;
+        int longest = 0;
+        int[] dp = new int[256];
+        for (int i = 0, j = 0, n = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (dp[c]++ == 0) {
+                for (++n; i <= j && n > k; i++) {
+                    if (--dp[s.charAt(i)] == 0) {
+                        n--;
+                    }
                 }
             }
-
             longest = Math.max(longest, j - i + 1);
         }
-
         return longest;
     }
 
--- test/src/main/java/com/inuker/test/main.java
+++ test/src/main/java/com/inuker/test/main.java
@@ -30,27 +30,20 @@ public class main {
         test.call("how");
     }
 
-    public List<Integer> topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
-        }
-        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return map.get(o1) - map.get(o2);
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int longest = 0;
+        int[] dp = new int[256];
+        for (int i = 0, j = 0, n = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (dp[c]++ == 0) {
+                for (++n; i <= j && n > k; i++) {
+                    if (--dp[s.charAt(i)] == 0) {
+                        n--;
+                    }
+                }
             }
-        });
-        for (Integer n : map.keySet()) {
-            queue.add(n);
-            if (queue.size() > k) {
-                queue.poll();
-            }
+            longest = Math.max(longest, j - i + 1);
         }
-        List<Integer> list = new LinkedList<>();
-        while (!queue.isEmpty()) {
-            list.add(queue.poll());
-        }
-        return list;
+        return longest;
     }
 }
