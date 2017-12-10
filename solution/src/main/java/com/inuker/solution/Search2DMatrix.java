c5423a82e7fb985593872aaf7882e0ce66efdd24
dingjikerbo <dingjikerbo@gmail.com>
2017-12-10 08:54:44 +0800

    fd

--- solution/src/main/java/com/inuker/solution/Search2DMatrix.java
+++ solution/src/main/java/com/inuker/solution/Search2DMatrix.java
@@ -8,33 +8,24 @@ import java.util.Arrays;
 
 public class Search2DMatrix {
 
-    // 耗时14ms
+    // 耗时11ms
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        int left = 0, right = row - 1;
-
-        while (left < right) {
-            int mid = (left + right) / 2 + 1;
-
-            int n = matrix[mid][0];
-
-            if (target == n) {
+        if (matrix.length == 0) {
+            return false;
+        }
+        int row = matrix.length, col = matrix[0].length;
+        int left = 0, right = row * col - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int x = mid / col, y = mid % col;
+            if (target == matrix[x][y]) {
                 return true;
-            } else if (target < n) {
-                right = mid - 1;
+            } else if (target > matrix[x][y]) {
+                left = mid + 1;
             } else {
-                left = mid;
+                right = mid - 1;
             }
         }
-
-        if (target < matrix[right][0]) {
-            return false;
-        } else if (target == matrix[right][0]) {
-            return true;
-        }
-
-        return Arrays.binarySearch(matrix[right], 0, col, target) >= 0;
+        return false;
     }
 }
