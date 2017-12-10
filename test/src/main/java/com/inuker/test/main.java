d56217db5c766b7a5de0301dc2dc03abad59b3d0
dingjikerbo <dingjikerbo@gmail.com>
2017-12-10 12:06:19 +0800

    fd

--- test/src/main/java/com/inuker/test/main.java
+++ test/src/main/java/com/inuker/test/main.java
@@ -38,4 +38,23 @@ public class main {
             System.out.print(n + " ");
         }
     }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i < s.length(); i++) {
+            for (String word: wordDict) {
+                int j = i - word.length();
+                if (j >= 0 && dp[j] && s.substring(j, i).equals(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
 }
