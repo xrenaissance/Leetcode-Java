10fd019b54aaad9f1172688966cef1bf7323b12a
dingjikerbo <dingjikerbo@gmail.com>
2018-09-06 07:03:50 +0800

    fd

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -4,24 +4,48 @@ import java.util.Stack;
 
 public class Main {
 
-    public TreeNode trimBST(TreeNode root, int L, int R) {
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+
+        Stack<TreeNode> stack1 = new Stack<TreeNode>();
+        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+
+        while (!stack1.isEmpty() || left != null) {
+            if (left != null) {
+                if (right == null) {
+                    return false;
+                }
+                if (left.val != right.val) {
+                    return false;
+                }
+                stack1.push(left);
+                stack2.push(right);
+                left = left.left;
+                right = right.right;
+            } else {
+                if (right != null) {
+                    return false;
+                }
+                if (stack2.isEmpty()) {
+                    return false;
+                }
+                left = stack1.pop().right;
+                right = stack2.pop().left;
+            }
+        }
+        return stack2.isEmpty() && right == null;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
         if (root == null) {
-            return root;
+            return true;
         }
-
-        if (root.val > R) {
-            root.right = null;
-            return trimBST(root.left, L, R);
-        }
-
-        if (root.val < L) {
-            root.left = null;
-            return trimBST(root.right, L, R);
-        }
-
-        root.left = trimBST(root.left, L, R);
-        root.right = trimBST(root.right, L, R);
-        return root;
+        return isSymmetric(root.left, root.right);
     }
 
     public static void main(String[] args) {
