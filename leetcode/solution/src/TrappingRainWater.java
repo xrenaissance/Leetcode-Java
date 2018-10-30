2b11bc62c99b66b850900781337c0c911568fcd0
dingjikerbo <dingjikerbo@gmail.com>
2018-10-30 17:10:10 +0800

    fd

--- leetcode/solution/src/TrappingRainWater.java
+++ leetcode/solution/src/TrappingRainWater.java
@@ -5,31 +5,19 @@ public class TrappingRainWater {
 
     // 耗时24ms
     public int trap(int[] height) {
-        int len = height.length;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
 
-        if (len == 0) {
-            return 0;
-        }
-
-        int[] left = new int[len];
-        int[] right = new int[len];
-
-        left[0] = 0;
-        right[len - 1] = 0;
-
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i < height.length; i++) {
             left[i] = Math.max(left[i - 1], height[i - 1]);
         }
-        for (int i = len - 2; i>= 0; i--) {
+        for (int i = height.length - 2; i >= 0; i--) {
             right[i] = Math.max(right[i + 1], height[i + 1]);
         }
-        int sum = 0;
-        for (int i = 0; i < len; i++) {
-            int high = Math.min(left[i], right[i]);
-            if (high > height[i]) {
-                sum += high - height[i];
-            }
+        int rain = 0;
+        for (int i = 1; i < height.length - 1; i++) {
+            rain += Math.max(0, Math.min(left[i], right[i]) - height[i]);
         }
-        return sum;
+        return rain;
     }
 }
--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -2,69 +2,23 @@ import java.util.*;
 
 public class Main {
 
-    public  String longestPalindrome(String s) {
-        int[] res = new int[2];
-        for (int i = 0; i < s.length(); i++) {
-            helper(s, i - 1, i + 1, res);
-            helper(s, i, i + 1, res);
-        }
-        return s.substring(res[1], res[1] + res[0]);
-    }
+    public int trap(int[] height) {
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
 
-    private void helper(String s, int left, int right, int[] res) {
-        for ( ; left >= 0 && right < s.length(); left--, right++) {
-            if (s.charAt(left) != s.charAt(right)) {
-                break;
-            }
+        for (int i = 1; i < height.length; i++) {
+            left[i] = Math.max(left[i - 1], height[i - 1]);
         }
-        int len = --right - ++left + 1;
-        if (len > res[0]) {
-            res[0] = len;
-            res[1] = left;
+        for (int i = height.length - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], height[i + 1]);
         }
+        int rain = 0;
+        for (int i = 1; i < height.length - 1; i++) {
+            rain += Math.max(0, Math.min(left[i], right[i]) - height[i]);
+        }
+        return rain;
     }
-
-//    public static String longestPalindrome(String s) {
-//        if (s.length() == 0) {
-//            return "";
-//        }
-//        int max = 0, idx = -1;
-//        for (int i = 0; i < s.length(); i++) {
-//            int len = findLen(s, i);
-//            if (len > max) {
-//                max = len;
-//                idx = i;
-//            }
-//        }
-//        if (max % 2 == 0) {
-//            return s.substring(idx - max / 2 + 1, idx + max / 2 + 1);
-//        } else {
-//            return s.substring(idx - max / 2, idx + max / 2 + 1);
-//        }
-//    }
-//
-//    private static int findLen(String s, int index) {
-//        int len1 = 1;
-//        for (int i = 1; i <= s.length(); i++) {
-//            int i1 = index - i, i2 = index + i;
-//            if (i1 < 0 || i2 >= s.length() || s.charAt(i1) != s.charAt(i2)) {
-//                break;
-//            }
-//            len1 += 2;
-//        }
-//        int len2 = 0;
-//        for (int i = 0; i < s.length(); i++) {
-//            int i1 = index - i, i2 = index + i + 1;
-//            if (i1 < 0 || i2 >= s.length() || s.charAt(i1) != s.charAt(i2)) {
-//                break;
-//            }
-//            len2 += 2;
-//        }
-//        return Math.max(len1, len2);
-//    }
 
     public static void main(String[] args) {
-        String s = longestPalindrome("cbbd");
-        System.out.println(s);
     }
 }
