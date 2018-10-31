0b43489aa0ea6566e0117046fb925696bdd82aeb
dingjikerbo <dingjikerbo@gmail.com>
2018-11-01 06:59:27 +0800

    fd

--- leetcode/solution/src/MergeKSortedList.java
+++ leetcode/solution/src/MergeKSortedList.java
@@ -8,7 +8,7 @@ import java.util.PriorityQueue;
 public class MergeKSortedList {
 
     // 耗时19ms
-    // 时间复杂度为O(knlgn)
+    // 时间复杂度为O(knlgn)，空间复杂度O(k)
     /**
      * 这里要注意lists中可能有node为null
      */
@@ -37,6 +37,45 @@ public class MergeKSortedList {
             }
         }
 
+        return dummy.next;
+    }
+
+    /**
+     * 第二种方法
+     * 时间复杂度O(Nlgk)，空间O(l)
+     * 耗时10ms
+     */
+    public ListNode mergeKLists2(ListNode[] lists) {
+        return helper(lists, 0, lists.length - 1);
+    }
+
+    private ListNode helper(ListNode[] lists, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode l1 = helper(lists, start, mid);
+        ListNode l2 = helper(lists, mid + 1, end);
+        return mergeTwoLists(l1, l2);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = l1, q = l2, cur = dummy;
+        for ( ; p != null && q != null; ) {
+            if (p.val < q.val) {
+                cur.next = p;
+                p = p.next;
+            } else {
+                cur.next = q;
+                q = q.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = p != null ? p : q;
         return dummy.next;
     }
 }
--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -2,23 +2,38 @@ import java.util.*;
 
 public class Main {
 
-    public int maxSubArray(int[] nums) {
-        int[] dp = new int[nums.length];
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = Math.max(i > 0 ? dp[i - 1] + nums[i] : nums[i], nums[i]);
-            max = Math.max(max, dp[i]);
-        }
-        return max;
+    public ListNode mergeKLists(ListNode[] lists) {
+        return helper(lists, 0, lists.length - 1);
     }
 
-    public int maxSubArray2(int[] nums) {
-        int max = Integer.MIN_VALUE, prev = 0;
-        for (int i = 0; i < nums.length; i++) {
-            prev = Math.max(i > 0 ? prev + nums[i] : nums[i], nums[i]);
-            max = Math.max(max, prev);
+    private ListNode helper(ListNode[] lists, int start, int end) {
+        if (start > end) {
+            return null;
         }
-        return max;
+        if (start == end) {
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode l1 = helper(lists, start, mid);
+        ListNode l2 = helper(lists, mid + 1, end);
+        return mergeTwoLists(l1, l2);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = l1, q = l2, cur = dummy;
+        for ( ; p != null && q != null; ) {
+            if (p.val < q.val) {
+                cur.next = p;
+                p = p.next;
+            } else {
+                cur.next = q;
+                q = q.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = p != null ? p : q;
+        return dummy.next;
     }
 
     public static void main(String[] args) {
