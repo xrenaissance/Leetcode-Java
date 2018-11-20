8e8a182fbfbae48fbaa2949f33c926135b9a3248
dingjikerbo <dingjikerbo@gmail.com>
2018-11-20 18:56:24 +0800

    fd

--- leetcode/solution/src/FriendCircles.java
+++ leetcode/solution/src/FriendCircles.java
@@ -23,4 +23,47 @@ public class FriendCircles {
             }
         }
     }
+
+    public int findCircleNum2(int[][] M) {
+        int n = M.length;
+        UnionFind uf = new UnionFind(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (M[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return uf.count();
+    }
+
+    class UnionFind {
+        int[] path;
+        int count;
+
+        public UnionFind(int n) {
+            path = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                path[i] = i;
+            }
+        }
+
+        public int find(int i) {
+            while (i != path[i]) i = path[i];
+            return i;
+        }
+
+        public void union(int i, int j) {
+            int rootI = find(i);
+            int rootJ = find(j);
+            if (rootI == rootJ) return;
+            path[rootI] = rootJ;
+            count--;
+        }
+
+        public int count() {
+            return count;
+        }
+    }
 }
