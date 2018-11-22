ea1019d66107a6125eb725ee459caf5771f99c4d
dingjikerbo <dingjikerbo@gmail.com>
2018-11-22 15:02:55 +0800

    fd

--- leetcode/solution/src/BSTIterator.java
+++ leetcode/solution/src/BSTIterator.java
@@ -6,12 +6,8 @@ public class BSTIterator {
     private TreeNode mCurNode;
 
     public BSTIterator(TreeNode root) {
-        mStack = new Stack<TreeNode>();
+        mStack = new Stack<>();
         mCurNode = root;
-
-//        for (mCurNode = root; mCurNode != null; mCurNode = mCurNode.left) {
-//            mStack.push(mCurNode);
-//        }
     }
 
     /**
@@ -41,19 +37,4 @@ public class BSTIterator {
 
         return result;
     }
-
-    /** 这样写也好，不过要注意在构造函数中初始化如上注释部分
-    public int next() {
-        if (mCurNode == null) {
-            mCurNode = mStack.pop();
-        }
-
-        int val = mCurNode.val;
-
-        for (mCurNode = mCurNode.right; mCurNode != null; mCurNode = mCurNode.left) {
-            mStack.push(mCurNode);
-        }
-
-        return val;
-    }*/
 }
--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -2,6 +2,25 @@ import java.util.*;
 
 public class Main {
 
+    public double myPow(double x, int n) {
+        return myPow(x, n);
+    }
+
+    private double myPow(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return myPow(1 / x, -n);
+        }
+        double res = myPow(x, n / 2);
+        if (n % 2 != 0) {
+            return res * res * x;
+        } else {
+            return res * res;
+        }
+    }
+
     public static void main(String[] args) {
     }
 }
