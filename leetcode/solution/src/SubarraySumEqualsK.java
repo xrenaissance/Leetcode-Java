5e9b951ae6bb105b69d3cbe23ee2a18d23ac7ca5
dingjikerbo <dingjikerbo@gmail.com>
2018-11-24 19:16:55 +0800

    fd

--- leetcode/solution/src/SubarraySumEqualsK.java
+++ leetcode/solution/src/SubarraySumEqualsK.java
@@ -8,6 +8,7 @@ public class SubarraySumEqualsK {
 
     /**
      * 注意map.put(0,1)，即当前数自己也算是一种
+     * TestCase [1], 0，即k=0的情况
      */
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -15,6 +16,8 @@ public class SubarraySumEqualsK {
         int count = 0;
         for (int i = 0, sum = 0; i < nums.length; i++) {
             sum += nums[i];
+            // 以下两句顺序非常重要，因为两个差要至少相差一个数以上，因此不能刚给当前sum放到Map就参与运算
+            // 比如k=0，如果刚给sum放到map就参与运算，那么sum-sum=0的情况是非法的
             count += map.getOrDefault(sum - k, 0);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -4,53 +4,16 @@ public class Main {
 
     public static class Solution {
 
-
-    }
-
-    class RandomizedSet {
-
-        ArrayList<Integer> list;
-        HashMap<Integer, Integer> map;
-        Random random;
-
-        /** Initialize your data structure here. */
-        public RandomizedSet() {
-            map = new HashMap<>();
-            list = new ArrayList<>();
-            random = new Random();
-        }
-
-        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-        public boolean insert(int val) {
-            if (map.containsKey(val)) {
-                return false;
+        public int subarraySum(int[] nums, int k) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+            int count = 0;
+            for (int i = 0, sum = 0; i < nums.length; i++) {
+                sum += nums[i];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+                count += map.getOrDefault(sum - k, 0);
             }
-            list.add(val);
-            map.put(val, list.size() - 1);
-            return true;
-        }
-
-        /** Removes a value from the set. Returns true if the set contained the specified element. */
-        public boolean remove(int val) {
-            int index = map.getOrDefault(val, -1);
-            if (index < 0) {
-                return false;
-            }
-            if (index != list.size() - 1) {
-                int tail = list.get(list.size() - 1);
-                list.set(index, tail);
-                map.put(tail, index);
-            }
-            map.remove(val);
-            list.remove(list.size() - 1);
-            return true;
-        }
-
-        /** Get a random element from the set. */
-        public int getRandom() {
-            int index = random.nextInt() % list.size();
-            index = (index >= 0 ? index : -index);
-            return list.get(index);
+            return count;
         }
     }
 
