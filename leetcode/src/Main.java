4ce3026b4aef3a26351702ead94f778b341f6f12
dingjikerbo <dingjikerbo@gmail.com>
2018-12-09 16:38:37 +0800

    fd

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -6,27 +6,18 @@ public class Main {
 
     public static class Solution {
 
-        public List<List<Integer>> pathSum(TreeNode root, int sum) {
-            List<List<Integer>> result = new ArrayList<>();
-            helpr(root, sum, new ArrayList<>(), result);
-            return result;
+        public boolean isSubtree(TreeNode s, TreeNode t) {
+            return isEqual(s, t) || (s != null && (isSubtree(s.left, t) || isSubtree(s.right, t)));
         }
 
-        private void helpr(TreeNode root, int sum, List<Integer> list, List<List<Integer>> result) {
-            if (root == null) {
-                return;
+        private boolean isEqual(TreeNode s, TreeNode t) {
+            if (t == null && s == null) {
+                return true;
             }
-
-            list.add(root.val);
-
-            if (root.left == null && root.right == null && root.val == sum) {
-                result.add(new ArrayList<>(list));
-            } else {
-                helpr(root.left, sum - root.val, list, result);
-                helpr(root.right, sum - root.val, list, result);
+            if (t == null || s == null) {
+                return false;
             }
-
-            list.remove(list.size() - 1);
+            return s.val == t.val && isEqual(s.left, t.left) && isEqual(s.right, t.right);
         }
     }
 
